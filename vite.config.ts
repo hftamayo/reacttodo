5a90be558c52eaf18a464bd14d6bc872227e43e8
Herbert Tamayo <hftamayo@gmail.com>
2024-10-22 08:49:54 -0600

    minor update on vite setup file

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     open: true,
-    host: "0.0.0.0",
+    host: '0.0.0.0',
   },
   resolve: {
     alias: {
-      "@": "/src",
+      '@': '/src',
     },
+  },
+  build: {
+    outDir: 'dist',
   },
 });
