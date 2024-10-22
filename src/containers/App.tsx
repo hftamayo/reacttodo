0a2c016cf618443936af653499831598a893b4aa
Herbert Tamayo <hftamayo@gmail.com>
2024-10-22 08:37:37 -0600

    testing version of main components

---
+++ src/containers/App.tsx
@@ -0,0 +1,12 @@
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1 className="text-4xl font-bold">Hello, World!</h1>
+      <p className="text-lg bg-blue-500 text-white p-6 rounded-2xl">
+        Brought to you by Tamayo and Co
+      </p>
+    </div>
+  );
+};
+
+export default App;
