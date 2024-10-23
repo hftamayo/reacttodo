833039546176a85a326f7ccc35bcbd49d1071f20
Herbert Tamayo <hftamayo@gmail.com>
2024-10-23 08:56:53 -0600

    tailwind styles for taskBoard

--- src/shared/utils/twind/styles.ts
+++ src/shared/utils/twind/styles.ts
@@ -6,3 +6,14 @@ export const taskRow = {
   textComplete: 'ml-2 cursor-pointer line-through',
   button: 'cursor-pointer flex items-center',
 };
+
+export const taskBoard = {
+  bg: 'h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]',
+  container:
+    'bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4',
+  heading: 'text-3xl font-bold text-center text-gray-800 p-2',
+  form: 'flex justify-between',
+  input: 'border p-2 w-full text-xl',
+  button: 'border p-4 ml-2 bg-purple-500 text-slate-100',
+  count: 'text-center p-2',
+};
