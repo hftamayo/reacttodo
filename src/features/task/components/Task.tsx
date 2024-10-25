64902b2ffa8cc4533985c69405187a1ddab10c33
Herbert Tamayo <hftamayo@gmail.com>
2024-10-25 08:13:17 -0600

    adding styles to task component

---
+++ src/features/task/components/Task.tsx
@@ -0,0 +1,32 @@
+import React, { FC } from 'react';
+import { FaRegTrashAlt } from 'react-icons/fa';
+import Label from '../../../shared/components/ui/Label';
+import Input from '../../../shared/components/ui/Input';
+import Button from '../../../shared/components/ui/Button';
+import { TaskProps } from '../../../shared/types/task.type';
+import { taskRow } from '../../../shared/utils/twind/styles';
+
+const Todo: FC<TaskProps> = ({ todo, toggleComplete, deleteTask }) => {
+  return (
+    <li className={todo.completed ? taskRow.liComplete : taskRow.li}>
+      <div className="flex">
+        <Input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={() => toggleComplete(todo)}
+        />
+        <Label
+          className={todo.completed ? taskRow.textComplete : taskRow.text}
+          onClick={() => toggleComplete(todo)}
+        >
+          {todo.text}
+        </Label>
+      </div>
+      <Button onClick={() => deleteTask(todo.id)}>
+        <FaRegTrashAlt />
+      </Button>
+    </li>
+  );
+};
+
+export default Todo;
