4b23ca40e0531def3075c50ffbef40f8905c258d
Herbert Tamayo <hftamayo@gmail.com>
2024-10-31 10:20:11 -0600

    useGetTasks and useGetTask finished, working on useAddTask

--- src/shared/services/api/tasks/taskHooks.ts
+++ src/shared/services/api/tasks/taskHooks.ts
@@ -1,24 +1,36 @@
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  UseMutationOptions,
+} from '@tanstack/react-query';
 import { taskService } from './taskService';
 import { TaskProps, TaskResponse } from '../../../types/task.type';
 
 const useGetTasks = () => {
-  return useQuery<TaskProps[], Error>('tasks', taskService.fetchTasks);
+  return useQuery<TaskResponse, Error>({
+    queryKey: ['tasks'],
+    queryFn: taskService.fetchTasks,
+  });
 };
 
 const useGetTask = (id: string) => {
-  return useQuery<TaskProps, Error>(['task', id], () =>
-    taskService.fetchTask(id)
-  );
+  return useQuery<TaskResponse, Error>({
+    queryKey: ['task', id],
+    queryFn: () => taskService.fetchTask(id),
+  });
 };
 
 const useAddTask = () => {
   const queryClient = useQueryClient();
-  return useMutation<TaskProps, Error, TaskProps>(taskService.fetchAddTask, {
-    onSuccess: () => {
-      queryClient.invalidateQueries('tasks');
-    },
-  });
+  return useMutation<TaskResponse, Error, TaskProps>(
+    (newTask: TaskProps) => taskService.fetchAddTask(newTask),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['tasks']);
+      },
+    }
+  );
 };
 
 const useUpdateTask = () => {
