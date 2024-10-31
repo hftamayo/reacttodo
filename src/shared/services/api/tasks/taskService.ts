f2ab06367b0c05505847f4c1fbc46d4063d3c5ac
Herbert Tamayo <hftamayo@gmail.com>
2024-10-31 07:50:59 -0600

    decoupling business logic into taskService

---
+++ src/shared/services/api/tasks/taskService.ts
@@ -0,0 +1,71 @@
+import { BACKEND_URL } from '../../../utils/envvars';
+import { TaskProps, TaskResponse } from '../../../types/task.type';
+
+const fetchTasks = async (): Promise<TaskProps[]> => {
+  const response = await fetch(`${BACKEND_URL}/tasks/all`, {
+    credentials: 'include',
+  });
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
+const fetchTask = async (id: string): Promise<TaskProps> => {
+  const response = await fetch(`${BACKEND_URL}/tasks/task/${id}`, {
+    credentials: 'include',
+  });
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
+const fetchAddTask = async (task: TaskProps): Promise<TaskProps> => {
+  const response = await fetch(`${BACKEND_URL}/tasks/task`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(task),
+  });
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
+const fetchUpdateTask = async (task: TaskProps): Promise<TaskProps> => {
+  const response = await fetch(`${BACKEND_URL}/tasks/task/${task.id}`, {
+    method: 'PUT',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(task),
+  });
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
+const fetchDeleteTask = async (id: string): Promise<TaskResponse> => {
+  const response = await fetch(`${BACKEND_URL}/tasks/task/${id}`, {
+    method: 'DELETE',
+    credentials: 'include',
+  });
+  if (!response.ok) {
+    throw new Error('No response received');
+  }
+  return response.json();
+};
+
+export const taskService = {
+  fetchTasks,
+  fetchTask,
+  fetchAddTask,
+  fetchUpdateTask,
+  fetchDeleteTask,
+};
