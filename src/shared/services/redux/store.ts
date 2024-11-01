e76bac05d94c766d9ad56ffb51594b8006daf005
Herbert Tamayo <hftamayo@gmail.com>
2024-11-01 12:02:18 -0600

    fixing an import

--- src/shared/services/redux/store.ts
+++ src/shared/services/redux/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { rootReducer } from './rootReducer';
+import rootReducer from './rootSlice';
 
 const store = configureStore({
   reducer: rootReducer,
