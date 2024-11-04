e886a2f6cd7b786d6bf0d0e5c3b4e2b719e222fb
Herbert Tamayo <hftamayo@gmail.com>
2024-11-04 09:24:11 -0600

    updates on addTask, updateTask and deleteTask methods

--- src/shared/services/redux/taskSlice.ts
+++ src/shared/services/redux/taskSlice.ts
@@ -46,6 +46,57 @@ export const getTask = createAsyncThunk(
   }
 );
 
+export const addTask = createAsyncThunk(
+  'task/addTask',
+  async (task: AddTaskProps, { rejectWithValue }) => {
+    try {
+      const response: TaskResponse = await taskService.fetchAddTask(task);
+      return response.task;
+    } catch (error) {
+      const apiError: ApiError = error as ApiError;
+      return rejectWithValue(
+        apiError.response
+          ? apiError.response.resultMessage
+          : 'An unknown error occurred. Please check your network connection and try again.'
+      );
+    }
+  }
+);
+
+export const updateTask = createAsyncThunk(
+  'task/updateTask',
+  async (task: TaskProps, { rejectWithValue }) => {
+    try {
+      const response: TaskResponse = await taskService.fetchUpdateTask(task);
+      return response.task;
+    } catch (error) {
+      const apiError: ApiError = error as ApiError;
+      return rejectWithValue(
+        apiError.response
+          ? apiError.response.resultMessage
+          : 'An unknown error occurred. Please check your network connection and try again.'
+      );
+    }
+  }
+);
+
+export const deleteTask = createAsyncThunk(
+  'task/deleteTask',
+  async (id: string, { rejectWithValue }) => {
+    try {
+      const response: TaskResponse = await taskService.fetchDeleteTask(id);
+      return response.task;
+    } catch (error) {
+      const apiError: ApiError = error as ApiError;
+      return rejectWithValue(
+        apiError.response
+          ? apiError.response.resultMessage
+          : 'An unknown error occurred. Please check your network connection and try again.'
+      );
+    }
+  }
+);
+
 const initialState: TasksState = {
   tasks: [],
   task: null,
