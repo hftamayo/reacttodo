bf14229ae5b8a05dafb256b611081aa377bdf67d
Herbert Tamayo <hftamayo@gmail.com>
2024-11-08 11:12:49 -0600

    first approach to TaskRow component

--- src/features/task/components/TaskRow.tsx
+++ src/features/task/components/TaskRow.tsx
@@ -1,28 +1,43 @@
-import React, { FC } from 'react';
+import React from 'react';
+import { useAppDispatch } from '../../../shared/services/redux/storeHooks';
+import { TaskProps } from '../../../shared/types/task.type';
+import {
+  updateTask,
+  deleteTask,
+} from '../../../shared/services/redux/taskSlice';
 import { FaRegTrashAlt } from 'react-icons/fa';
 import Label from '../../../shared/components/ui/Label';
 import Input from '../../../shared/components/ui/Input';
 import Button from '../../../shared/components/ui/Button';
-import { TaskProps } from '../../../shared/types/task.type';
 import { taskRow } from '../../../shared/utils/twind/styles';
 
-const TaskRow: FC<TaskProps> = ({ todo, toggleComplete, deleteTask }) => {
+const TaskRow: React.FC<TaskProps> = (todo: TaskProps) => {
+  const dispatch = useAppDispatch();
+
+  const handleToggleComplete = () => {
+    dispatch(updateTask({ ...todo, complete: !todo.complete }));
+  };
+
+  const handleDeleteTask = () => {
+    dispatch(deleteTask(todo.id!));
+  };
+
   return (
-    <li className={todo.completed ? taskRow.liComplete : taskRow.li}>
+    <li className={todo.complete ? taskRow.liComplete : taskRow.li}>
       <div className="flex">
         <Input
           type="checkbox"
-          checked={todo.completed}
-          onChange={() => toggleComplete(todo)}
+          checked={todo.complete}
+          onChange={handleToggleComplete}
         />
         <Label
-          className={todo.completed ? taskRow.textComplete : taskRow.text}
-          onClick={() => toggleComplete(todo)}
+          className={todo.complete ? taskRow.textComplete : taskRow.text}
+          onClick={handleToggleComplete}
         >
-          {todo.text}
+          {todo.name}
         </Label>
       </div>
-      <Button onClick={() => deleteTask(todo.id)}>
+      <Button onClick={handleDeleteTask}>
         <FaRegTrashAlt />
       </Button>
     </li>
