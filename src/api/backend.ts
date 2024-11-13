92f01a0986dbed98a118c5dcdf4730b9436e7be2
Herbert Tamayo <hftamayo@gmail.com>
2024-11-13 12:49:48 -0600

    ensuring type enforce during backend methods

--- src/api/backend.ts
+++ src/api/backend.ts
@@ -1,5 +1,5 @@
 import { BACKEND_URL } from '../shared/utils/envvars';
-import { ApiError, TaskResponse } from '../shared/types/task.type';
+import { ApiError, TaskProps, TaskResponse } from '../shared/types/task.type';
 
 export const taskOps = {
   async getTasks(): Promise<TaskResponse> {
@@ -19,23 +19,82 @@ export const taskOps = {
     }
   },
 
-  async getTask(id: string) {
-    const response = await instance.get(`${BACKEND_URL}/tasks/task/${id}`);
-    return response.data;
+  async getTask(id: string): Promise<TaskResponse> {
+    try {
+      const response = await fetch(`${BACKEND_URL}/tasks/task/${id}`, {
+        credentials: 'include',
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return await response.json();
+    } catch (error: unknown) {
+      const apiError = error as ApiError;
+      throw new Error(
+        apiError.response?.resultMessage || 'An unknown error occurred'
+      );
+    }
   },
-  async addTask(role: any) {
-    const response = await instance.post(`${BACKEND_URL}/tasks/task`, role);
-    return response.data;
+
+  async addTask(task: TaskProps): Promise<TaskResponse> {
+    try {
+      const response = await fetch(`${BACKEND_URL}/tasks/task`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(task),
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return await response.json();
+    } catch (error: unknown) {
+      const apiError = error as ApiError;
+      throw new Error(
+        apiError.response?.resultMessage || 'An unknown error occurred'
+      );
+    }
   },
-  async updateTask(role: any) {
-    const response = await instance.put(
-      `${BACKEND_URL}/tasks/task/${role.id}`,
-      role
-    );
-    return response.data;
+
+  async updateTask(task: TaskProps): Promise<TaskResponse> {
+    try {
+      const response = await fetch(`${BACKEND_URL}/tasks/task/${task.id}`, {
+        method: 'PUT',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(task),
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return await response.json();
+    } catch (error: unknown) {
+      const apiError = error as ApiError;
+      throw new Error(
+        apiError.response?.resultMessage || 'An unknown error occurred'
+      );
+    }
   },
-  async deleteTask(id: string) {
-    const response = await instance.delete(`${BACKEND_URL}/tasks/task/${id}`);
-    return response.data;
+
+  async deleteTask(id: string): Promise<TaskResponse> {
+    try {
+      const response = await fetch(`${BACKEND_URL}/tasks/task/${id}`, {
+        method: 'DELETE',
+        credentials: 'include',
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return await response.json();
+    } catch (error: unknown) {
+      const apiError = error as ApiError;
+      throw new Error(
+        apiError.response?.resultMessage || 'An unknown error occurred'
+      );
+    }
   },
 };
