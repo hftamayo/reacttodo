02886678a8a7db59b89c6a41ffe76f5295582681
Herbert Tamayo <hftamayo@gmail.com>
2024-11-14 07:26:49 -0600

    trying to find the component name for input

--- src/shared/components/ui/Input.tsx
+++ src/shared/components/ui/Input.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { Input as ShadcnInput } from '@shadcn/ui';
+import { FormInput as ShadcnInput } from '@shadcn/ui';
 import { InputProps } from '../../types/shadcn-component.type';
 
 const Input: FC<Readonly<InputProps>> = ({ ...props }) => {
