302b5aa123fc9ec8697d6dc99815035d837e22f6
Herbert Tamayo <hftamayo@gmail.com>
2024-11-15 07:39:47 -0600

    enabling redux store and query client

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
-import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './shared/services/redux/store';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import TaskBoard from './containers/TaskBoard';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <TaskBoard />
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <TaskBoard />
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>
 );
