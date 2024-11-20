f259e600310ddcf864a348e6ba6fd95fac950bba
Herbert Tamayo <hftamayo@gmail.com>
2024-11-20 08:46:29 -0600

    removing unneeded css tags

--- src/shared/components/ui/input.tsx
+++ src/shared/components/ui/input.tsx
@@ -19,7 +19,7 @@ const inputVariants = cva(
     compoundVariants: [
       { type: 'text', ctrlsize: 'small', className: 'h-6 text-sm' },
       { type: 'text', ctrlsize: 'medium', className: 'h-9 text-base' },
-      { type: 'text', ctrlsize: 'large', className: 'h-12 text-lg flex-grow' },
+      { type: 'text', ctrlsize: 'large', className: 'h-12 text-lg' },
       { type: 'checkbox', ctrlsize: 'small', className: 'h-3 w-3' },
       { type: 'checkbox', ctrlsize: 'medium', className: 'h-4 w-4' },
       { type: 'checkbox', ctrlsize: 'large', className: 'h-5 w-5' },
