0d3986df7aaf523215f6fb829538881c1ab55ca9
Herbert Tamayo <hftamayo@gmail.com>
2024-11-21 13:47:36 -0600

    updating css for taskboard container

--- src/shared/utils/twind/styles.ts
+++ src/shared/utils/twind/styles.ts
@@ -8,6 +8,7 @@ export const taskRow = {
 };
 
 export const taskBoard = {
+  boardContainer: 'flex items-center justify-center min-h-screen',
   bg: 'h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]',
   container:
     'bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4',
