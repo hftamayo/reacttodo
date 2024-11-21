477abe837c4d53bd0170187ed1b041abbe5de35e
Herbert Tamayo <hftamayo@gmail.com>
2024-11-21 12:58:27 -0600

    rearranging some tags

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,18 @@ import store from './shared/services/redux/store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import TaskBoard from './containers/TaskBoard';
 import './index.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+import { taskBoard } from './shared/utils/twind/styles';
 
 const queryClient = new QueryClient();
 
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <TaskBoard />
+        <div className={taskBoard.boardContainer}>
+          <TaskBoard />
+        </div>
       </QueryClientProvider>
     </Provider>
   </React.StrictMode>
