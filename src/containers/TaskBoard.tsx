d8a54b9473075c0e72ba63c9670b0de73cc3125b
Herbert Tamayo <hftamayo@gmail.com>
2024-11-22 08:10:43 -0600

    using sonner to display errors

--- src/containers/TaskBoard.tsx
+++ src/containers/TaskBoard.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AddTaskForm from '../features/task/components/AddTaskForm';
 import TaskRow from '../features/task/components/TaskRow';
 import { taskBoard } from '@/shared/utils/twind/styles';
 import { taskHooks } from '@/shared/services/api/tasks/taskHooks';
 import useLazyLoad from '@/shared/services/lazyloading/hooks/useLazyLoad';
 import { APP_NAME } from '@/shared/utils/envvars';
+import { toast } from 'sonner';
 
 const TaskBoard: React.FC = () => {
   const { ref, shouldFetch } = useLazyLoad();
@@ -15,13 +16,18 @@ const TaskBoard: React.FC = () => {
   const totalTasks = tasks.length;
   const completedTasks = tasks.filter((task) => task.complete).length;
 
+  useEffect(() => {
+    if (error) {
+      toast.error(error.message);
+    }
+  }, [error]);
+
   return (
     <div className={taskBoard.bg}>
       <div className={taskBoard.container}>
         <h3 className={taskBoard.heading}>{APP_NAME}</h3>
         <AddTaskForm />
         {isLoading && <p>Loading...</p>}
-        {error && <p>Error: {error.message}</p>}
         <ul>
           {tasks.map((task) => (
             <TaskRow key={task.id} {...task} />
