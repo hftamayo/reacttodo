947363c1c12e4accf8195c69c0f8afb749d748fe
Herbert Tamayo <hftamayo@gmail.com>
2024-11-29 08:04:16 -0600

    scaffold for DashboardHeader

--- src/features/dashboard/components/DashBoardHeader.tsx
+++ src/features/dashboard/components/DashBoardHeader.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { FaBars, FaSearch, FaBell, FaUserCircle } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+import { APP_NAME } from '@/shared/utils/envvars';
+import MenuItem from '@/shared/components/ui/dashboard/MenuItem';
 
 const DashBoardHeader = () => {
   return <div>DashBoardHeader</div>;
