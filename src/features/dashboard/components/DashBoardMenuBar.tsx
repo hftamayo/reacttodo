cecddb9eb5375bd45b7dee571c4afade7a90ac3e
Herbert Tamayo <hftamayo@gmail.com>
2024-12-04 09:40:56 -0600

    decoupling to DropDown and MenuItem components

--- src/features/dashboard/components/DashBoardMenuBar.tsx
+++ src/features/dashboard/components/DashBoardMenuBar.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import DropDownMenu from './menu/DropDownMenu';
+import MenuItem from './menu/MenuItem';
+import { MenuProps } from '@/shared/types/menu.type';
+import { DashBoardMenuBarStyles } from '@/shared/utils/twind/styles';
 
-const DashBoardMenuBar = () => {
-  return <div>DashBoardMenuBar</div>;
+const DashBoardMenuBar: React.FC<MenuProps> = ({ options }) => {
+  return (
+    <div>
+      <ul className={DashBoardMenuBarStyles.div2ndLevelContainer}>
+        {options.map((option) => (
+          <MenuItem key={option.path} option={option} />
+        ))}
+      </ul>
+
+      <div className={DashBoardMenuBarStyles.div2ndLevelContainer}>
+        <ul className={DashBoardMenuBarStyles.unorderedItem2ndLevel}>
+          <DropDownMenu options={options} />
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default DashBoardMenuBar;
