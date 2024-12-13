12d1a1a16e337bfdff6a0db80e5cec8ce027b40d
Herbert Tamayo <hftamayo@gmail.com>
2024-12-13 12:33:19 -0600

    adding ToggleMenu on MainLayout

--- src/shared/components/ui/dashboard/layout/MainLayout.tsx
+++ src/shared/components/ui/dashboard/layout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CustomOutlet from '@/shared/services/routing/CustomOutlet';
 import DashBoardHeader from '@/features/dashboard/components/header/DashBoardHeader';
+import DashBoardToggleMenuBar from '@/features/dashboard/components/header/menu/DashBoardToggleMenuBar';
 import DashBoardFooter from '@/features/dashboard/components/footer/DashBoardFooter';
 import { MainLayoutStyles } from '@/shared/utils/twind/styles';
 
@@ -12,10 +13,13 @@ const MainLayout = () => {
       <div className={MainLayoutStyles.layoutHeader}>
         {isAuthenticated ? <DashBoardHeader /> : null}
       </div>
-      <div
-        className={`${MainLayoutStyles.layoutContent} flex items-center justify-center overflow-hidden`}
-      >
-        <CustomOutlet />
+      <div className="flex">
+        <DashBoardToggleMenuBar />
+        <div
+          className={`${MainLayoutStyles.layoutContent} flex items-center justify-center overflow-hidden`}
+        >
+          <CustomOutlet />
+        </div>
       </div>
       <div className={MainLayoutStyles.layoutFooter}>
         {isAuthenticated ? <DashBoardFooter /> : null}
