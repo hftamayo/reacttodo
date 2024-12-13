e1e16c2f5638814709e3601be687ad6e7e2694e3
Herbert Tamayo <hftamayo@gmail.com>
2024-12-13 12:06:39 -0600

    custom outlet for dynamic routing render

---
+++ src/shared/services/routing/CustomOutlet.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useRoute, Switch, Route } from 'wouter';
+import DashBoardAnalytics from '@/features/dashboard/components/content/DashBoardAnalytics';
+import TaskBoard from '@/containers/TaskBoard';
+
+const CustomOutlet: React.FC = () => {
+  const [match] = useRoute('/');
+
+  return (
+    <Switch>
+      {match && <Route path="/" component={DashBoardAnalytics} />}
+      <Route path="/tasksboard" component={TaskBoard} />
+      {/* Add more routes as needed */}
+    </Switch>
+  );
+};
+
+export default CustomOutlet;
