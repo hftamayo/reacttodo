4b75fab0eb9e6938398686549c6a83f7098b114c
Herbert Tamayo <hftamayo@gmail.com>
2024-12-13 12:07:03 -0600

    assenbling content in main layout

---
+++ src/shared/components/ui/dashboard/layout/MainLayout.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import CustomOutlet from '@/shared/services/routing/CustomOutlet';
+import DashBoardHeader from '@/features/dashboard/components/header/DashBoardHeader';
+import DashBoardFooter from '@/features/dashboard/components/footer/DashBoardFooter';
+import { MainLayoutStyles } from '@/shared/utils/twind/styles';
+
+const MainLayout = () => {
+  const isAuthenticated = true;
+
+  return (
+    <div className={MainLayoutStyles.layoutContainer}>
+      <div className={MainLayoutStyles.layoutHeader}>
+        {isAuthenticated ? <DashBoardHeader /> : null}
+      </div>
+      <div
+        className={`${MainLayoutStyles.layoutContent} flex items-center justify-center overflow-hidden`}
+      >
+        <CustomOutlet />
+      </div>
+      <div className={MainLayoutStyles.layoutFooter}>
+        {isAuthenticated ? <DashBoardFooter /> : null}
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
