141cc2ffa181e6f33e6ba2c080ae98249e2e9942
Herbert Tamayo <hftamayo@gmail.com>
2024-12-13 12:29:52 -0600

    decoupling menuBar from header

--- src/features/dashboard/components/header/DashBoardHeader.tsx
+++ src/features/dashboard/components/header/DashBoardHeader.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import DashBoardToggleMenuBar from './menu/DashBoardToggleMenuBar';
 import ContainerActions from './actions/ContainerActions';
 
 const DashBoardHeader: React.FC = () => {
   return (
     <div className="flex">
       <ContainerActions />
-      <DashBoardToggleMenuBar />
     </div>
   );
 };
