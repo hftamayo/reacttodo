b2aaf7ec4238aca2d31c9302e33cb16ca2a36f95
Herbert Tamayo <hftamayo@gmail.com>
2024-12-18 09:51:15 -0600

    adding more elements to the skeleton dashboard

--- src/shared/components/ui/dashboard/skeleton/SkeletonDashBoardAnalytics.tsx
+++ src/shared/components/ui/dashboard/skeleton/SkeletonDashBoardAnalytics.tsx
@@ -4,9 +4,17 @@ const SkeletonDashBoardAnalytics: React.FC = () => {
   return (
     <div className="bg-gray-200 p-4 rounded-md shadow-md">
       <h2 className="text-xl font-bold mb-4">DashBoard Analytics</h2>
-      <div className="h-32 bg-gray-300 rounded-md mb-4"></div>
-      <div className="h-32 bg-gray-300 rounded-md mb-4"></div>
-      <div className="h-32 bg-gray-300 rounded-md"></div>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="h-32 bg-gray-300 rounded-md"></div>
+        <div className="h-32 bg-gray-300 rounded-md"></div>
+        <div className="h-32 bg-gray-300 rounded-md"></div>
+        <div className="h-32 bg-gray-300 rounded-md"></div>
+        <div className="h-32 bg-gray-300 rounded-md"></div>
+        <div className="h-32 bg-gray-300 rounded-md"></div>
+        <div className="h-32 bg-gray-300 rounded-md"></div>
+        <div className="h-32 bg-gray-300 rounded-md"></div>
+        <div className="h-32 bg-gray-300 rounded-md"></div>
+      </div>
     </div>
   );
 };
