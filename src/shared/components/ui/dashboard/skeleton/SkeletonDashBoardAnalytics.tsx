5c97be89a8a449dc3485106c4e84461096910f2c
Herbert Tamayo <hftamayo@gmail.com>
2024-12-18 09:56:20 -0600

    improving boxes size

--- src/shared/components/ui/dashboard/skeleton/SkeletonDashBoardAnalytics.tsx
+++ src/shared/components/ui/dashboard/skeleton/SkeletonDashBoardAnalytics.tsx
@@ -5,15 +5,15 @@ const SkeletonDashBoardAnalytics: React.FC = () => {
     <div className="bg-gray-200 p-4 rounded-md shadow-md">
       <h2 className="text-xl font-bold mb-4">DashBoard Analytics</h2>
       <div className="grid grid-cols-3 gap-4">
-        <div className="h-32 bg-gray-300 rounded-md"></div>
-        <div className="h-32 bg-gray-300 rounded-md"></div>
-        <div className="h-32 bg-gray-300 rounded-md"></div>
-        <div className="h-32 bg-gray-300 rounded-md"></div>
-        <div className="h-32 bg-gray-300 rounded-md"></div>
-        <div className="h-32 bg-gray-300 rounded-md"></div>
-        <div className="h-32 bg-gray-300 rounded-md"></div>
-        <div className="h-32 bg-gray-300 rounded-md"></div>
-        <div className="h-32 bg-gray-300 rounded-md"></div>
+        <div className="w-64 h-32 bg-gray-300 rounded-md"></div>
+        <div className="w-64 h-32 bg-gray-300 rounded-md"></div>
+        <div className="w-64 h-32 bg-gray-300 rounded-md"></div>
+        <div className="w-64 h-32 bg-gray-300 rounded-md"></div>
+        <div className="w-64 h-32 bg-gray-300 rounded-md"></div>
+        <div className="w-64 h-32 bg-gray-300 rounded-md"></div>
+        <div className="w-64 h-32 bg-gray-300 rounded-md"></div>
+        <div className="w-64 h-32 bg-gray-300 rounded-md"></div>
+        <div className="w-64 h-32 bg-gray-300 rounded-md"></div>
       </div>
     </div>
   );
