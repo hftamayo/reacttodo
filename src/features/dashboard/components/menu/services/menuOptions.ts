94dbf72291d61f010081345f7e35f0b2053692d5
Herbert Tamayo <hftamayo@gmail.com>
2024-12-18 08:39:15 -0600

    adding a spacer to each menuoptions group

--- src/features/dashboard/components/menu/services/menuOptions.ts
+++ src/features/dashboard/components/menu/services/menuOptions.ts
@@ -7,6 +7,7 @@ import {
 } from 'react-icons/fa';
 
 export const adminMenuOptions = [
+  { path: '', label: '', icon: FaHome },
   { path: '/dashboard/home', label: 'Home', icon: FaHome },
   { path: '/dashboard/entities/roles', label: 'Roles', icon: FaRegFileAlt },
   { path: '/dashboard/entities/users', label: 'Users', icon: FaRegFileAlt },
@@ -17,6 +18,7 @@ export const adminMenuOptions = [
 ];
 
 export const userMenuOptions = [
+  { path: '', label: '', icon: FaHome },
   { path: '/dashboard/home', label: 'Home', icon: FaHome },
   { path: '/taskboard', label: 'Tasks', icon: FaRegFileAlt },
   { path: '/dashboard/inbox', label: 'Inbox', icon: FaRegEnvelope },
@@ -24,6 +26,7 @@ export const userMenuOptions = [
 ];
 
 export const supervisorMenuOptions = [
+  { path: '', label: '', icon: FaHome },
   { path: '/dashboard/home', label: 'Home', icon: FaHome },
   { path: '/dashboard/entities/users', label: 'Users', icon: FaRegFileAlt },
   { path: '/taskboard', label: 'Tasks', icon: FaRegFileAlt },
