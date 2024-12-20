f86748753cca595b26298ef18b9e6ea05af5bf6f
Herbert Tamayo <hftamayo@gmail.com>
2024-12-20 08:15:03 -0600

    updating rootSlicer with themeReducer

--- src/shared/services/redux/rootSlice.ts
+++ src/shared/services/redux/rootSlice.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import taskReducer from './taskSlice';
+import themeReducer from './themeSlice';
 
 const rootReducer = combineReducers({
   task: taskReducer,
+  theme: themeReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
