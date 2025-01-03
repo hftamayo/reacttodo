4c0d22a9613c20746a1f528716d755c46b64c3c4
Herbert Tamayo <hftamayo@gmail.com>
2025-01-03 15:31:30 -0600

    adding skeleton dboardAnalytics

--- src/shared/services/routing/CustomOutlet.tsx
+++ src/shared/services/routing/CustomOutlet.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useRoute, Switch, Route } from 'wouter';
-import DashBoardAnalytics from '@/features/dashboard/components/content/DashBoardAnalytics';
+import SkeletonDashBoardAnalytics from '@/shared/components/ui/dashboard/skeleton/SkeletonDashBoardAnalytics';
 import TaskBoard from '@/containers/TaskBoard';
 
 const CustomOutlet: React.FC = () => {
@@ -8,7 +8,7 @@ const CustomOutlet: React.FC = () => {
 
   return (
     <Switch>
-      {match && <Route path="/" component={DashBoardAnalytics} />}
+      {match && <Route path="/" component={SkeletonDashBoardAnalytics} />}
       <Route path="/taskboard" component={TaskBoard} />
       {/* Add more routes as needed */}
     </Switch>
