9735bd7a14b2d521c820db73c686ceec4793b8cb
Herbert Tamayo <hftamayo@gmail.com>
2025-01-03 15:30:35 -0600

    updating home endpoint

--- src/features/dashboard/components/menu/services/menuOptions.ts
+++ src/features/dashboard/components/menu/services/menuOptions.ts
@@ -8,7 +8,7 @@ import {
 
 export const adminMenuOptions = [
   { path: '', label: '', icon: FaHome },
-  { path: '/dashboard/home', label: 'Home', icon: FaHome },
+  { path: '/', label: 'Home', icon: FaHome },
   { path: '/dashboard/entities/roles', label: 'Roles', icon: FaRegFileAlt },
   { path: '/dashboard/entities/users', label: 'Users', icon: FaRegFileAlt },
   { path: '/taskboard', label: 'Tasks', icon: FaRegFileAlt },
@@ -19,7 +19,7 @@ export const adminMenuOptions = [
 
 export const userMenuOptions = [
   { path: '', label: '', icon: FaHome },
-  { path: '/dashboard/home', label: 'Home', icon: FaHome },
+  { path: '/', label: 'Home', icon: FaHome },
   { path: '/taskboard', label: 'Tasks', icon: FaRegFileAlt },
   { path: '/dashboard/inbox', label: 'Inbox', icon: FaRegEnvelope },
   { path: '/dashboard/reports', label: 'Reports', icon: FaPoll },
@@ -27,7 +27,7 @@ export const userMenuOptions = [
 
 export const supervisorMenuOptions = [
   { path: '', label: '', icon: FaHome },
-  { path: '/dashboard/home', label: 'Home', icon: FaHome },
+  { path: '/', label: 'Home', icon: FaHome },
   { path: '/dashboard/entities/users', label: 'Users', icon: FaRegFileAlt },
   { path: '/taskboard', label: 'Tasks', icon: FaRegFileAlt },
   { path: '/dashboard/inbox', label: 'Inbox', icon: FaRegEnvelope },
