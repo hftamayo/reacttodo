70961e9a8830d620d86469035a77c95078456f8d
Herbert Tamayo <hftamayo@gmail.com>
2025-01-09 12:46:31 -0600

    collecting available messages to translate

---
+++ src/shared/utils/languages/stringMessages.ts
@@ -0,0 +1,106 @@
+import { LanguageContextType } from '@/shared/types/ui.type';
+
+const stringMessages: LanguageContextType = {
+  //dashboard:
+  dropDownHeaderBar: {
+    en: {
+      group: {
+        profile: 'Profile',
+        settings: 'Settings',
+        logout: 'Logout',
+      },
+    },
+    es: {
+      group: {
+        profile: 'Perfil',
+        settings: 'Configuración',
+        logout: 'Cerrar sesión',
+      },
+    },
+    de: {
+      group: {
+        profile: 'Profil',
+        settings: 'Einstellungen',
+        logout: 'Ausloggen',
+      },
+    },
+  },
+
+  sideBarDashboard: {
+    en: {
+      group: {
+        home: 'Home',
+        roles: 'Roles',
+        users: 'Users',
+        tasks: 'Tasks',
+        reports: 'Reports',
+        inbox: 'Inbox',
+        settings: 'Settings',
+      },
+    },
+    es: {
+      group: {
+        home: 'Inicio',
+        roles: 'Roles',
+        users: 'Usuarios',
+        tasks: 'Tareas',
+        reports: 'Informes',
+        inbox: 'Mensajeria',
+        settings: 'Configuraciones',
+      },
+    },
+    de: {
+      group: {
+        home: 'Zuhause',
+        roles: 'Rollen',
+        users: 'Benutzer',
+        tasks: 'Aufgaben',
+        reports: 'Berichte',
+        inbox: 'Posteingang',
+        settings: 'Einstellungen',
+      },
+    },
+  },
+
+  statusBarDashboard: {
+    en: {
+      group: {
+        status: 'Status',
+        suggestions: 'Suggestions',
+        service: 'Service Desk',
+      },
+    },
+    es: {
+      group: {
+        status: 'Estado',
+        suggestions: 'Sugerencias',
+        service: 'Service Desk',
+      },
+    },
+    de: {
+      group: {
+        status: 'Status',
+        suggestions: 'Vorschläge',
+        service: 'Service Desk',
+      },
+    },
+  },
+
+  //forms:
+  confirmDialogDeleteEntity: {
+    en: {
+      title: 'Confirm',
+      text: 'Are you sure you want to proceed?',
+    },
+    es: {
+      title: 'Confirmar',
+      text: '¿Estás seguro de que quieres continuar?',
+    },
+    de: {
+      title: 'Bestätigen',
+      text: 'Sind Sie sicher, dass Sie fortfahren möchten?',
+    },
+  },
+};
+
+export default stringMessages;
