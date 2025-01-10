1ef1c3b7a0f7ee9f522f7caca8a83933b96e8a40
Herbert Tamayo <hftamayo@gmail.com>
2025-01-10 10:27:40 -0600

    adding a slicer for language management

---
+++ src/shared/services/redux/languageSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from './rootSlice';
+
+const initialState = {
+  language: localStorage.getItem('language') ?? 'en',
+};
+
+const languageSlice = createSlice({
+  name: 'language',
+  initialState,
+  reducers: {
+    setLanguage: (state, action) => {
+      state.language = action.payload;
+      localStorage.setItem('language', action.payload);
+    },
+  },
+});
+
+export const { setLanguage } = languageSlice.actions;
+export const selectLanguage = (state: RootState) => state.language.language;
+export default languageSlice.reducer;
