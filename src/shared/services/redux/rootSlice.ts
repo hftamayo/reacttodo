681c74048f7263ec399461835c046a219caa5036
Herbert Tamayo <hftamayo@gmail.com>
2025-01-10 11:56:26 -0600

    updating slicer path

--- src/shared/services/redux/rootSlice.ts
+++ src/shared/services/redux/rootSlice.ts
@@ -1,7 +1,7 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import taskReducer from './taskSlice';
-import themeReducer from './themeSlice';
-import languageReducer from './languageSlice';
+import taskReducer from './slices/taskSlice';
+import themeReducer from './slices/themeSlice';
+import languageReducer from './slices/languageSlice';
 
 const rootReducer = combineReducers({
   task: taskReducer,
