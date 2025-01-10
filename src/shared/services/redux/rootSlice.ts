72b8baffe0a1b34d378b1f2efc76c7907aa1db57
Herbert Tamayo <hftamayo@gmail.com>
2025-01-10 11:54:39 -0600

    adding languageSlicer to rootSlicer

--- src/shared/services/redux/rootSlice.ts
+++ src/shared/services/redux/rootSlice.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import taskReducer from './taskSlice';
 import themeReducer from './themeSlice';
+import languageReducer from './languageSlice';
 
 const rootReducer = combineReducers({
   task: taskReducer,
   theme: themeReducer,
+  language: languageReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
