2bdd707dc1e39f1aa8e947455d2f80445cd82899
Herbert Tamayo <hftamayo@gmail.com>
2025-01-13 08:39:59 -0600

    applying translation

--- src/features/dashboard/components/header/actions/ThemeButton.tsx
+++ src/features/dashboard/components/header/actions/ThemeButton.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Link } from 'wouter';
 import { FaToggleOn, FaToggleOff } from 'react-icons/fa';
 import { useAppDispatch } from '@/shared/services/redux/hooks/useAppDispatch';
 import { useAppSelector } from '@/shared/services/redux/hooks/useAppSelector';
 import { toggleTheme } from '@/shared/services/redux/slices/themeSlice';
 import { DashBoardHeaderActionsStyles } from '@/shared/utils/twind/styles';
+import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
 
 const ThemeButton: React.FC = () => {
   const dispatch = useAppDispatch();
   const theme = useAppSelector((state: any) => state.theme.theme);
+  const { text: textOn } = useTranslation('themeButtonOn');
+  const { text: textOff } = useTranslation('themeButtonOff');
 
   const handleToggle = () => {
     dispatch(toggleTheme());
@@ -20,11 +22,13 @@ const ThemeButton: React.FC = () => {
         <FaToggleOff
           className={DashBoardHeaderActionsStyles.toggleButton}
           onClick={handleToggle}
+          title={textOff}
         />
       ) : (
         <FaToggleOn
           className={DashBoardHeaderActionsStyles.toggleButton}
           onClick={handleToggle}
+          title={textOn}
         />
       )}
     </div>
