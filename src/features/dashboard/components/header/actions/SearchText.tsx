20be8990cad88089a4e70eca0dc068c723efa129
Herbert Tamayo <hftamayo@gmail.com>
2025-01-13 08:31:08 -0600

    applying translate to searchText

--- src/features/dashboard/components/header/actions/SearchText.tsx
+++ src/features/dashboard/components/header/actions/SearchText.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { DashBoardHeaderActionsStyles } from '@/shared/utils/twind/styles';
+import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
 
 const SearchText: React.FC = () => {
+  const { text } = useTranslation('searchTextControl');
+
   return (
     <div className="relative md:w-65">
       <span className={DashBoardHeaderActionsStyles.spanSearchAction}>
@@ -13,6 +16,7 @@ const SearchText: React.FC = () => {
       <input
         type="text"
         className={DashBoardHeaderActionsStyles.inputSearchAction}
+        title={text}
       />
     </div>
   );
