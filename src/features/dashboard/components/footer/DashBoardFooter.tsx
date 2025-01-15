fd3ff1e0527c34164c840cbb2ab47327325e26bc
Herbert Tamayo <hftamayo@gmail.com>
2025-01-15 09:15:25 -0600

    applying int to footer

--- src/features/dashboard/components/footer/DashBoardFooter.tsx
+++ src/features/dashboard/components/footer/DashBoardFooter.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { FaFacebook, FaTwitter, FaTiktok } from 'react-icons/fa';
 import { DashBoardFooterStyles } from '@/shared/utils/twind/styles';
+import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
 
 const DashBoardFooter = () => {
+  const { text: statusOn } = useTranslation('statusOn');
+  const { text: statusOff } = useTranslation('statusOff');
+  const { text: suggestions } = useTranslation('suggestions');
+  const { text: serviceDesk } = useTranslation('serviceDesk');
+
   return (
     <footer className="flex justify-between items-center bg-black text-white">
       <div className="flex items-center">
@@ -27,16 +33,13 @@ const DashBoardFooter = () => {
         </a>
       </div>
       <div className="flex">
-        <span className={DashBoardFooterStyles.footer_text}>
-          Status: Online
-        </span>{' '}
-        |
+        <span className={DashBoardFooterStyles.footer_text}>{statusOn}</span> |
         <a href="{}" className={DashBoardFooterStyles.footer_links}>
-          Suggestions
+          {suggestions}
         </a>{' '}
         |
         <a href="{}" className={DashBoardFooterStyles.footer_links}>
-          Service-Desk
+          {serviceDesk}
         </a>
       </div>
     </footer>
