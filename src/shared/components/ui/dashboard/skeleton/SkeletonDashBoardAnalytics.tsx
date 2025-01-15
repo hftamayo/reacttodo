a84bfd7c9bf60830238a1f138f58dfb4ba9dfb2d
Herbert Tamayo <hftamayo@gmail.com>
2025-01-15 09:07:27 -0600

    adding int to dboardSkleton

--- src/shared/components/ui/dashboard/skeleton/SkeletonDashBoardAnalytics.tsx
+++ src/shared/components/ui/dashboard/skeleton/SkeletonDashBoardAnalytics.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
 
 const SkeletonDashBoardAnalytics: React.FC = () => {
+  const { title } = useTranslation('dashboardAnalyticsForm');
+
   return (
     <div className="bg-gray-200 p-4 rounded-md shadow-md">
-      <h2 className="text-xl font-bold mb-4">DashBoard Analytics</h2>
+      <h2 className="text-xl font-bold mb-4">{title}</h2>
       <div className="grid grid-cols-3 gap-4">
         <div className="w-64 h-32 bg-gray-300 rounded-md"></div>
         <div className="w-64 h-32 bg-gray-300 rounded-md"></div>
