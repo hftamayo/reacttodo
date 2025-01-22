bb81ee5fb496777394ee9db9bad091d1c7e60cbd
Herbert Tamayo <hftamayo@gmail.com>
2025-01-22 08:54:54 -0600

    updating ViewSettingsForm

--- src/features/settings/components/ViewSettingsForm.tsx
+++ src/features/settings/components/ViewSettingsForm.tsx
@@ -7,22 +7,58 @@ import {
   CardHeader,
   CardTitle,
 } from '@/shared/components/ui/forms/card';
+import { Button } from '@/shared/components/ui/forms/button';
+import { Input } from '@/shared/components/ui/forms/input';
+import { Label } from '@/shared/components/ui/forms/label';
+import {
+  RadioGroup,
+  RadioGroupItem,
+} from '@/shared/components/ui/forms/radio-group';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/shared/components/ui/forms/select';
 import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
 
 const ViewSettingsForm: React.FC = () => {
-  const { title } = useTranslation('settingsForm');
+  const { title, text } = useTranslation('settingsForm');
 
   return (
     <Card>
       <CardHeader>
         <CardTitle>{title}</CardTitle>
-        <CardDescription>Card Description</CardDescription>
+        <CardDescription>{text}</CardDescription>
       </CardHeader>
       <CardContent>
-        <p>Card Content</p>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name of your project" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="framework">Framework</Label>
+              <Select>
+                <SelectTrigger id="framework">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  <SelectItem value="next">Next.js</SelectItem>
+                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                  <SelectItem value="astro">Astro</SelectItem>
+                  <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        </form>
       </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button variant="additive">Save</Button>
       </CardFooter>
     </Card>
   );
