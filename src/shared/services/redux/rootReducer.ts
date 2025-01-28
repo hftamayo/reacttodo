950e460937edd1e80ced4eb34c5b53112ee5da36
Herbert Tamayo <hftamayo@gmail.com>
2025-01-28 14:58:30 -0600

    named component on store

--- src/shared/services/redux/rootReducer.ts
+++ src/shared/services/redux/rootReducer.ts
@@ -3,12 +3,10 @@ import taskReducer from './slices/taskSlice';
 import themeReducer from './slices/themeSlice';
 import languageReducer from './slices/languageSlice';
 
-const rootReducer = combineReducers({
+export const rootReducer = combineReducers({
   task: taskReducer,
   theme: themeReducer,
   language: languageReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
-
-export default rootReducer;
--- src/shared/services/redux/store.ts
+++ src/shared/services/redux/store.ts
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './rootReducer';
+import { rootReducer } from './rootReducer';
 
-const store = configureStore({
+export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-
-export default store;
