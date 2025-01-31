75ee8d107731d42ebcca78810f9377843051cb50
Herbert Tamayo <hftamayo@gmail.com>
2025-01-31 08:44:11 -0600

    improving vertical position of TaskBoardPresenter

--- src/shared/utils/twind/styles.ts
+++ src/shared/utils/twind/styles.ts
@@ -58,10 +58,8 @@ export const DashBoardFooterStyles = {
 };
 
 export const taskBoard = {
-  boardContainer: 'flex items-center justify-center min-h-screen',
-  bg: 'flex items-center justify-center h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]',
-  container:
-    'bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4',
+  bg: 'flex justify-center w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]',
+  container: 'bg-slate-100 max-w-[500px] w-full rounded-md shadow-xl p-4 mt-4',
   heading: 'text-3xl font-bold text-center text-gray-800 p-2',
   form: 'flex justify-between',
   frmContainer: 'flex items-center space-x-4 w-full',
