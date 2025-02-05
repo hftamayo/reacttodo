0439a00647fbbb91aae8bb769a5ffcae43cf4de3
Herbert Tamayo <hftamayo@gmail.com>
2025-02-05 08:50:32 -0600

    adding skeleton component

---
+++ src/shared/components/ui/skeleton/Skeleton.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { cn } from '@/shared/components/ui/cn';
+
+const Skeleton: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
+  className,
+  ...props
+}) => {
+  return (
+    <div
+      className={cn('animate-pulse rounded-md bg-primary/10', className)}
+      {...props}
+    />
+  );
+};
+
+export { Skeleton };
