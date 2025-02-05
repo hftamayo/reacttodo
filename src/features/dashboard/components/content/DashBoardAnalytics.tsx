a2187e34001b786132e53f651a1e7f38320da0fb
Herbert Tamayo <hftamayo@gmail.com>
2025-02-05 09:05:06 -0600

    using skeletons at first point

--- src/features/dashboard/components/content/DashBoardAnalytics.tsx
+++ src/features/dashboard/components/content/DashBoardAnalytics.tsx
@@ -8,15 +8,76 @@ export const DashBoardAnalytics: React.FC = () => {
   return (
     <div className="bg-gray-200 p-4 rounded-md shadow-md">
       <h2 className="text-xl font-bold mb-4">{title}</h2>
-      <div className="grid grid-cols-3 gap-4">
-        <Skeleton className="w-64 h-32 " />
-        <Skeleton className="w-64 h-32 " />
-        <Skeleton className="w-64 h-32 " />
-        <Skeleton className="w-64 h-32 " />
-        <Skeleton className="w-64 h-32 " />
-        <Skeleton className="w-64 h-32 " />
-        <Skeleton className="w-64 h-32 " />
-        <Skeleton className="w-64 h-32 " />
+      <div className="grid grid-cols-3 gap-8">
+        <div className="space-y-8">
+          <div className="flex items-center space-x-4">
+            <Skeleton className="h-12 w-12 rounded-full" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="h-4 w-[200px]" />
+            </div>
+          </div>
+          <div className="flex items-center space-x-4">
+            <Skeleton className="h-12 w-12 rounded-full" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="h-4 w-[200px]" />
+            </div>
+          </div>
+          <div className="flex items-center space-x-4">
+            <Skeleton className="h-12 w-12 rounded-full" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="h-4 w-[200px]" />
+            </div>
+          </div>
+        </div>
+        <div className="space-y-8">
+          <div className="flex items-center space-x-4">
+            <Skeleton className="h-12 w-12 rounded-full" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="h-4 w-[200px]" />
+            </div>
+          </div>
+          <div className="flex items-center space-x-4">
+            <Skeleton className="h-12 w-12 rounded-full" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="h-4 w-[200px]" />
+            </div>
+          </div>
+          <div className="flex items-center space-x-4">
+            <Skeleton className="h-12 w-12 rounded-full" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="h-4 w-[200px]" />
+            </div>
+          </div>
+        </div>
+        <div className="space-y-8">
+          <div className="flex items-center space-x-4">
+            <Skeleton className="h-12 w-12 rounded-full" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="h-4 w-[200px]" />
+            </div>
+          </div>
+          <div className="flex items-center space-x-4">
+            <Skeleton className="h-12 w-12 rounded-full" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="h-4 w-[200px]" />
+            </div>
+          </div>
+          <div className="flex items-center space-x-4">
+            <Skeleton className="h-12 w-12 rounded-full" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="h-4 w-[200px]" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
