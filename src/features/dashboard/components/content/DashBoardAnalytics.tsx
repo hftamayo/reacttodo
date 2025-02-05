c36fbf5e38a17629ad4a63d927a0832fbc7b957e
Herbert Tamayo <hftamayo@gmail.com>
2025-02-05 08:57:49 -0600

    implementing shadcn skeleton control

--- src/features/dashboard/components/content/DashBoardAnalytics.tsx
+++ src/features/dashboard/components/content/DashBoardAnalytics.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
+import { Skeleton } from '@/shared/components/ui/skeleton/Skeleton';
 
-const DashBoardAnalytics = () => {
-  return <div>DashBoardAnalytics</div>;
+export const DashBoardAnalytics: React.FC = () => {
+  const { title } = useTranslation('dashboardAnalyticsForm');
+
+  return (
+    <div className="bg-gray-200 p-4 rounded-md shadow-md">
+      <h2 className="text-xl font-bold mb-4">{title}</h2>
+      <div className="grid grid-cols-3 gap-4">
+        <Skeleton className="w-64 h-32 " />
+        <Skeleton className="w-64 h-32 " />
+        <Skeleton className="w-64 h-32 " />
+        <Skeleton className="w-64 h-32 " />
+        <Skeleton className="w-64 h-32 " />
+        <Skeleton className="w-64 h-32 " />
+        <Skeleton className="w-64 h-32 " />
+        <Skeleton className="w-64 h-32 " />
+      </div>
+    </div>
+  );
 };
-
-export default DashBoardAnalytics;
