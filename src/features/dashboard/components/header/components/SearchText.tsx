2e89ce7d687d8bc913c204f9c30370458b36d95c
Herbert Tamayo <hftamayo@gmail.com>
2025-02-11 10:59:03 -0600

    updating searchText component

--- src/features/dashboard/components/header/components/SearchText.tsx
+++ src/features/dashboard/components/header/components/SearchText.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
+import { Input } from '@/shared/components/ui/input/Input';
 import { DashBoardHeaderActionsStyles } from '@/shared/utils/twind/styles';
 import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
 
 export const SearchText: React.FC = () => {
-  const { text } = useTranslation('searchTextControl');
+  const { text: textHolder } = useTranslation('searchTextControl');
+  const { text: textValidation } = useTranslation('searchTextValidation');
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSearch = (data: any) => {
+    console.log(data);
+  };
 
   return (
     <div className="relative md:w-65">
@@ -13,10 +20,12 @@ export const SearchText: React.FC = () => {
           <FaSearch />
         </button>
       </span>
-      <input
+      <Input
         type="text"
-        className={DashBoardHeaderActionsStyles.inputSearchAction}
-        title={text}
+        ctrlsize="large"
+        placeholder={textHolder}
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
       />
     </div>
   );
