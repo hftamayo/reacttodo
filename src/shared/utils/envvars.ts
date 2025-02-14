5ed18ecdc85f2b49946afca7ea9f38ba8614c5af
Herbert Tamayo <hftamayo@gmail.com>
2025-02-14 13:06:39 -0600

    removing backend type prop

--- src/shared/utils/envvars.ts
+++ src/shared/utils/envvars.ts
@@ -1,3 +1,2 @@
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL ?? '';
 export const APP_NAME = import.meta.env.VITE_APP_NAME ?? '';
-export const BACKEND_TYPE = import.meta.env.VITE_BACKEND_TYPE ?? '0';
