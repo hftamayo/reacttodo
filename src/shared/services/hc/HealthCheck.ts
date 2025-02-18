886a735873d67b149458421a960999a999f3bed9
Herbert Tamayo <hftamayo@gmail.com>
2025-02-18 10:13:22 -0600

    second group of updates on healthcheck

--- src/shared/services/hc/HealthCheck.ts
+++ src/shared/services/hc/HealthCheck.ts
@@ -7,42 +7,43 @@ import {
 } from '@/shared/types/api.type';
 import { useQueryClient } from '@tanstack/react-query';
 import { toast } from 'sonner';
+import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
 
-export const HealthCheck: React.FC = () => {
-  const [status, setStatus] = useState<string | null>(null);
-  const [error, setError] = useState<string | null>(null);
+export const HealthCheck = ({
+  setStatus,
+}: {
+  setStatus: (status: string) => void;
+}) => {
+  const [status, setStatusInternal] = useState<string | null>(null);
   const queryClient = useQueryClient();
+  const { text: statusOn } = useTranslation('statusOn');
+  const { text: statusOff } = useTranslation('statusOff');
 
   useEffect(() => {
     const checkHealth = async () => {
       try {
         const response: ApiResponse<HealthCheckData<AppHealthDetails>> =
           await beOps.appHealth();
+        setStatusInternal(response.data.healthCheck.status);
         setStatus(response.data.healthCheck.status);
-        setError(null);
       } catch (err: unknown) {
-        if (err instanceof Error) {
-          setError(err.message);
-          setStatus('fail');
-        } else {
-          setError(String(err));
-          setStatus('fail');
-        }
+        setStatusInternal('fail');
+        setStatus('fail');
       }
     };
 
     const intervalId = setInterval(checkHealth, 10000); // Check every 10 seconds
 
     return () => clearInterval(intervalId);
-  }, [queryClient]);
+  }, [queryClient, setStatus]);
 
   useEffect(() => {
     if (status === 'fail') {
-      toast.error('Connection lost');
+      toast.error(statusOff);
     } else if (status === 'pass') {
-      toast.success('Connection restored');
+      toast.error(statusOn);
     }
-  }, [status]);
+  }, [status, statusOn, statusOff]);
 
   return null; // This component does not render anything itself
 };
