abe092cc9ada1cb1aaf1c5db02f9958b39f8f877
Herbert Tamayo <hftamayo@gmail.com>
2025-02-18 12:58:44 -0600

    expanding errorUtils for showing toaster

--- src/shared/utils/error/errorUtils.ts
+++ src/shared/utils/error/errorUtils.ts
@@ -1,3 +1,5 @@
+import { toast } from 'sonner';
+import { toasterMessages } from '@/shared/utils/twind/styles';
 import { ApiError } from '@/shared/types/api.type';
 
 const isApiError = (error: any): error is ApiError => {
@@ -8,6 +10,16 @@ export const getErrorMessage = (error: ApiError | unknown): string => {
   if (isApiError(error)) {
     return error.resultMessage;
   }
+  if (error instanceof Error) {
+    return error.message;
+  }
   console.error('Unhandled error:', error); // Optional: Log the error for debugging
   return 'An unknown error occurred. Please check your network connection and try again.';
+};
+
+export const showError = (error: ApiError | unknown) => {
+  const errorMessage = getErrorMessage(error);
+  toast.error(errorMessage, {
+    className: toasterMessages.errorToaster,
+  });
 };
