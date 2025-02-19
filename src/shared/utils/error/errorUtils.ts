ba9f1057c5ba063ef3559d9180501873a755fe89
Herbert Tamayo <hftamayo@gmail.com>
2025-02-19 11:17:14 -0600

    cleaning up errorUtils

--- src/shared/utils/error/errorUtils.ts
+++ src/shared/utils/error/errorUtils.ts
@@ -2,22 +2,11 @@ import { toast } from 'sonner';
 import { toasterMessages } from '@/shared/utils/twind/styles';
 import { ApiError } from '@/shared/types/api.type';
 
-const isApiError = (error: any): error is ApiError => {
-  return error && typeof error === 'object' && 'resultMessage' in error;
+export const getErrorMessage = (error: ApiError): string => {
+  return `${error.httpStatusCode} ${error.resultMessage}`;
 };
 
-export const getErrorMessage = (error: ApiError | unknown): string => {
-  if (isApiError(error)) {
-    return error.resultMessage;
-  }
-  if (error instanceof Error) {
-    return error.message;
-  }
-  console.error('Unhandled error:', error); // Optional: Log the error for debugging
-  return 'An unknown error occurred. Please check your network connection and try again.';
-};
-
-export const showError = (error: ApiError | unknown, userMessage: string) => {
+export const showError = (error: ApiError, userMessage: string) => {
   const errorMessage = getErrorMessage(error);
   console.error('Error:', errorMessage); // Log detailed error for developers
   toast.error(userMessage || 'An error occurred. Please try again later.', {
