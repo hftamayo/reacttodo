b84aff3e58cb1c0380ae86942837bcee6e9d057a
Herbert Tamayo <hftamayo@gmail.com>
2025-02-20 11:10:27 -0600

    bringing again isApiError method

--- src/shared/utils/error/errorUtils.ts
+++ src/shared/utils/error/errorUtils.ts
@@ -2,6 +2,15 @@ import { toast } from 'sonner';
 import { toasterMessages } from '@/shared/utils/twind/styles';
 import { ApiError } from '@/shared/types/api.type';
 
+export const isApiError = (error: any): error is ApiError => {
+  return (
+    error &&
+    typeof error === 'object' &&
+    'httpStatusCode' in error &&
+    'resultMessage' in error
+  );
+};
+
 export const getErrorMessage = (error: ApiError): string => {
   return `${error.httpStatusCode} ${error.resultMessage}`;
 };
