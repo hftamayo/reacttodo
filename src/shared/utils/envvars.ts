84045cb99fb82c1238196e1e7f0bd1d264d10f7c
Herbert Tamayo <hftamayo@gmail.com>
2025-02-21 08:13:14 -0600

    adding constants for health check routine

--- src/shared/utils/envvars.ts
+++ src/shared/utils/envvars.ts
@@ -1,2 +1,7 @@
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL ?? '';
 export const APP_NAME = import.meta.env.VITE_APP_NAME ?? '';
+export const HEALTH_CHECK_INTERVAL =
+  import.meta.env.HEALTH_CHECK_INTERVAL ?? 60000;
+export const OFFLINE_CHECK_INTERVAL =
+  import.meta.env.OFFLINE_CHECK_INTERVAL ?? 5000;
+export const MAX_RETRIES = import.meta.env.MAX_RETRIES ?? 3;
