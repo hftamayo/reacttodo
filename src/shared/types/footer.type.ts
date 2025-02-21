b7b82e2165755e04bae7f071d7aaa59547e5b5df
Herbert Tamayo <hftamayo@gmail.com>
2025-02-21 08:49:10 -0600

    styles for footer

---
+++ src/shared/types/footer.type.ts
@@ -0,0 +1,12 @@
+import { IconType } from 'react-icons';
+
+export type SocialLink = {
+  icon: IconType;
+  url: string;
+  label: string;
+};
+
+export type FooterLink = {
+  href: string;
+  label: string;
+};
