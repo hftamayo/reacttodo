564cd91779a10abb848c89e30af3b794d52eeea9
Herbert Tamayo <hftamayo@gmail.com>
2025-02-24 14:30:45 -0600

    cleaning up dboardPresenter

--- src/containers/DashBoard/DashBoardAnalyticsPresenter.tsx
+++ src/containers/DashBoard/DashBoardAnalyticsPresenter.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { HealthMetrics } from '@/shared/types/healthcheck.type';
 import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
-import { Skeleton } from '@/shared/components/ui/skeleton/Skeleton';
 
 export const DashBoardAnalyticsPresenter: React.FC<{
   metrics: HealthMetrics;
@@ -11,77 +10,6 @@ export const DashBoardAnalyticsPresenter: React.FC<{
   return (
     <div className="bg-[var(--cameo-100)] p-4 rounded-md shadow-md">
       <h2 className="text-xl font-bold mb-4">{title}</h2>
-      <div className="grid grid-cols-3 gap-8">
-        <div className="space-y-8">
-          <div className="flex items-center space-x-4">
-            <Skeleton className="h-12 w-12 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px]" />
-              <Skeleton className="h-4 w-[200px]" />
-            </div>
-          </div>
-          <div className="flex items-center space-x-4">
-            <Skeleton className="h-12 w-12 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px]" />
-              <Skeleton className="h-4 w-[200px]" />
-            </div>
-          </div>
-          <div className="flex items-center space-x-4">
-            <Skeleton className="h-12 w-12 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px]" />
-              <Skeleton className="h-4 w-[200px]" />
-            </div>
-          </div>
-        </div>
-        <div className="space-y-8">
-          <div className="flex items-center space-x-4">
-            <Skeleton className="h-12 w-12 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px]" />
-              <Skeleton className="h-4 w-[200px]" />
-            </div>
-          </div>
-          <div className="flex items-center space-x-4">
-            <Skeleton className="h-12 w-12 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px]" />
-              <Skeleton className="h-4 w-[200px]" />
-            </div>
-          </div>
-          <div className="flex items-center space-x-4">
-            <Skeleton className="h-12 w-12 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px]" />
-              <Skeleton className="h-4 w-[200px]" />
-            </div>
-          </div>
-        </div>
-        <div className="space-y-8">
-          <div className="flex items-center space-x-4">
-            <Skeleton className="h-12 w-12 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px]" />
-              <Skeleton className="h-4 w-[200px]" />
-            </div>
-          </div>
-          <div className="flex items-center space-x-4">
-            <Skeleton className="h-12 w-12 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px]" />
-              <Skeleton className="h-4 w-[200px]" />
-            </div>
-          </div>
-          <div className="flex items-center space-x-4">
-            <Skeleton className="h-12 w-12 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px]" />
-              <Skeleton className="h-4 w-[200px]" />
-            </div>
-          </div>
-        </div>
-      </div>
       <div className="mt-8">
         <h3 className="text-lg font-semibold">Health Metrics</h3>
         <div className="flex flex-col items-start">
