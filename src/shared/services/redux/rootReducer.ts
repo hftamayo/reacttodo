c51e3cd33831b09d62044f87cdbc8a46baeaad26
Herbert Tamayo <hftamayo@gmail.com>
2025-02-24 12:23:53 -0600

    adding healthMetricsReducer to rootReducer

--- src/shared/services/redux/rootReducer.ts
+++ src/shared/services/redux/rootReducer.ts
@@ -2,11 +2,13 @@ import { combineReducers } from '@reduxjs/toolkit';
 import taskReducer from '../../../features/task/store/taskSlice';
 import themeReducer from './slices/themeSlice';
 import languageReducer from './slices/languageSlice';
+import healthMetricsReducer from './slices/healthMetricsSlice';
 
 export const rootReducer = combineReducers({
   task: taskReducer,
   theme: themeReducer,
   language: languageReducer,
+  healthMetrics: healthMetricsReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
