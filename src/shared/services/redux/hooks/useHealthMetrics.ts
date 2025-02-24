879157fbec24743aab42193a388ba7877f79fa2e
Herbert Tamayo <hftamayo@gmail.com>
2025-02-24 14:23:52 -0600

    decoupling metrics data fetching using a hook

---
+++ src/shared/services/redux/hooks/useHealthMetrics.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch } from '@/shared/services/redux/store';
+import { RootState } from '@/shared/services/redux/rootReducer';
+import { fetchHealthMetrics } from '@/shared/services/redux/slices/healthMetricsSlice';
+
+export const useHealthMetrics = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const metrics = useSelector((state: RootState) => state.healthMetrics);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadMetrics = async () => {
+      try {
+        await dispatch(fetchHealthMetrics()).unwrap();
+        setLoading(false);
+      } catch (error) {
+        console.error('Failed to load health metrics:', error);
+        setLoading(false);
+      }
+    };
+
+    loadMetrics();
+  }, [dispatch]);
+
+  return { metrics, loading };
+};
