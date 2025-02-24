ef36c5f4616e2cb9d51b72b6e3a6fb4ed58d5a30
Herbert Tamayo <hftamayo@gmail.com>
2025-02-24 13:54:09 -0600

    handling startTime performance from the health slicer

--- src/shared/services/redux/slices/healthMetricsSlice.ts
+++ src/shared/services/redux/slices/healthMetricsSlice.ts
@@ -1,5 +1,6 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
 import { HealthMetrics } from '@/shared/types/healthcheck.type';
+import { beOps } from '../../api/apiClient';
 
 const initialState: HealthMetrics = {
   lastCheckTime: Date.now(),
@@ -8,6 +9,22 @@ const initialState: HealthMetrics = {
   responseTime: 0,
 };
 
+export const fetchHealthMetrics = createAsyncThunk(
+  'healthMetrics/fetchHealthMetrics',
+  async () => {
+    const response = await beOps.appHealth();
+    const endTime = performance.now();
+    const responseTime =
+      endTime - (response.data.healthCheck.details?.startTime ?? 0);
+    return {
+      lastCheckTime: Date.now(),
+      failureCount: 0,
+      averageResponseTime: responseTime,
+      responseTime,
+    };
+  }
+);
+
 const healthMetricsSlice = createSlice({
   name: 'healthMetrics',
   initialState,
@@ -15,6 +32,11 @@ const healthMetricsSlice = createSlice({
     setMetrics(state, action: PayloadAction<HealthMetrics>) {
       return action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchHealthMetrics.fulfilled, (state, action) => {
+      return action.payload;
+    });
   },
 });
 
