101d9efbf19e95d49e9c889a29dff29fdbbfaddc
Herbert Tamayo <hftamayo@gmail.com>
2025-02-24 12:06:38 -0600

    slicer for health metrics

---
+++ src/shared/services/redux/slices/healthMetricsSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { HealthMetrics } from '@/shared/types/healthcheck.type';
+
+const initialState: HealthMetrics = {
+  lastCheckTime: Date.now(),
+  failureCount: 0,
+  averageResponseTime: 0,
+};
+
+const healthMetricsSlice = createSlice({
+  name: 'healthMetrics',
+  initialState,
+  reducers: {
+    setMetrics(state, action: PayloadAction<HealthMetrics>) {
+      return action.payload;
+    },
+  },
+});
+
+export const { setMetrics } = healthMetricsSlice.actions;
+export default healthMetricsSlice.reducer;
