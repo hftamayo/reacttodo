850ab8293024848ca6323f264020236f452658db
Herbert Tamayo <hftamayo@gmail.com>
2025-02-26 09:42:24 -0600

    adding memoizing and erro caching during the data handshaking

--- src/containers/TaskBoard/TaskBoardPresenter.tsx
+++ src/containers/TaskBoard/TaskBoardPresenter.tsx
@@ -1,24 +1,47 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useMemo } from 'react';
 import { AddTaskForm } from '@/features/task/components/AddTaskForm';
 import { TaskRow } from '@/features/task/components/TaskRow';
 import { taskBoard } from '@/shared/utils/twind/styles';
 import { APP_NAME } from '@/shared/utils/envvars';
 import { TaskBoardPresenterProps } from '@/shared/types/api.type';
+import { showUIError } from '@/shared/utils/error/errorUtils';
 
 const TaskBoardPresenter = forwardRef<HTMLDivElement, TaskBoardPresenterProps>(
-  ({ tasks, isLoading, totalTasks, completedTasks }, ref) => {
+  ({ tasks, isLoading, totalTasks, completedTasks, error }, ref) => {
+    const taskList = useMemo(() => {
+      if (error) {
+        showUIError(error.message);
+        return (
+          <p className={taskBoard.error}>
+            Failed to load data. Incident reported
+          </p>
+        );
+      }
+
+      if (isLoading) {
+        return <p>Loading data...</p>;
+      }
+
+      if (tasks.length === 0) {
+        return <p>No ongoing tasks</p>;
+      }
+
+      return (
+        <ul>
+          {tasks.map((task) => (
+            <TaskRow key={task.id} {...task} />
+          ))}
+        </ul>
+      );
+    }, [tasks, isLoading, error]);
+
     return (
       <div className={taskBoard.bg}>
         <div className={taskBoard.container}>
           <h3 className={taskBoard.heading}>{APP_NAME}</h3>
           <AddTaskForm />
-          {isLoading && <p>Loading data...</p>}
-          <ul>
-            {tasks.map((task) => (
-              <TaskRow key={task.id} {...task} />
-            ))}
-          </ul>
-          {totalTasks < 1 ? null : (
+          {taskList}
+          {totalTasks > 0 && (
             <p className={taskBoard.count}>
               {`You have ${totalTasks} tasks, ${completedTasks} completed`}
             </p>
@@ -30,4 +53,4 @@ const TaskBoardPresenter = forwardRef<HTMLDivElement, TaskBoardPresenterProps>(
   }
 );
 
-export default TaskBoardPresenter;
+export default React.memo(TaskBoardPresenter);
