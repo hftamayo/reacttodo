840597c54ab9ef3e5943a6c2ad6c0443a4dcaccc
Herbert Tamayo <hftamayo@gmail.com>
2025-02-27 10:45:33 -0600

    removing method for error notification

--- src/shared/services/hc/notificationService.ts
+++ src/shared/services/hc/notificationService.ts
@@ -1,14 +1,8 @@
 import { toast } from 'sonner';
 import { toasterMessages } from '@/shared/utils/twind/styles';
-import { showApiError } from '@/shared/utils/error/errorUtils';
 
 export const showSuccessToast = (message: string) => {
   toast.success(message, {
     className: toasterMessages.successToaster,
   });
-};
-
-export const showErrorToast = (message: string, userMessage: string) => {
-  console.log('Message to the developer: ', message);
-  showApiError({ httpStatusCode: 500, resultMessage: message }, userMessage);
 };
