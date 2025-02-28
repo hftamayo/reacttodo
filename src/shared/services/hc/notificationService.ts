685fa407a89dd993e4165de2ebdef25562954897
Herbert Tamayo <hftamayo@gmail.com>
2025-02-28 11:54:07 -0600

    moving notifService to subdir

--- src/shared/services/hc/notificationService.ts
+++
@@ -1,8 +0,0 @@
-import { toast } from 'sonner';
-import { toasterMessages } from '@/shared/utils/twind/styles';
-
-export const showSuccessToast = (message: string) => {
-  toast.success(message, {
-    className: toasterMessages.successToaster,
-  });
-};
