9f4235772df9cdd6e38795e912c7f1a37ec02f59
Herbert Tamayo <hftamayo@gmail.com>
2025-03-03 15:13:04 -0600

    updating the flow to obtain healthCheck analytics

--- src/containers/DashBoard/DashBoardAnalyticsContainer.tsx
+++ src/containers/DashBoard/DashBoardAnalyticsContainer.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useHealthMetrics } from '@/features/healthcheck/hooks/useHealthMetrics';
+import { useHealthCheck } from '@/features/healthcheck/hooks/useHealthCheck';
 import { DashBoardAnalyticsPresenter } from './DashBoardAnalyticsPresenter';
 import { DashBoardAnalyticsSkeleton } from '@/shared/components/ui/skeleton/DashBoardAnalyticsSkeleton';
 
 export const DashBoardAnalyticsContainer: React.FC = () => {
-  const { metrics, loading } = useHealthMetrics();
+  const metrics = useHealthCheck();
+  const loading = metrics.status === 'CHECKING';
 
   if (loading) {
     return <DashBoardAnalyticsSkeleton />;
--- src/containers/DashBoard/DashBoardAnalyticsPresenter.tsx
+++ src/containers/DashBoard/DashBoardAnalyticsPresenter.tsx
@@ -7,12 +7,20 @@ export const DashBoardAnalyticsPresenter: React.FC<{
 }> = ({ metrics }) => {
   const { title } = useTranslation('dashboardAnalyticsForm');
 
+  const statusText = {
+    ONLINE: 'System Operational',
+    OFFLINE: 'System Unavailable',
+    NO_CONNECTION: 'No Connection',
+    CHECKING: 'Checking Status...',
+  }[metrics.status];
+
   return (
     <div className="bg-[var(--cameo-100)] p-4 rounded-md shadow-md">
       <h2 className="text-xl font-bold mb-4">{title}</h2>
       <div className="mt-8">
         <h3 className="text-lg font-semibold">Health Metrics</h3>
         <div className="flex flex-col items-start">
+          <span>Status: {statusText}</span>
           <span>
             Last Check: {new Date(metrics.lastCheckTime).toLocaleString()}
           </span>
@@ -21,6 +29,9 @@ export const DashBoardAnalyticsPresenter: React.FC<{
             Avg Response Time: {metrics.averageResponseTime.toFixed(2)} ms
           </span>
           <span>Last Response Time: {metrics.responseTime.toFixed(2)} ms</span>
+          <span>
+            Network Connection: {metrics.isOnline ? 'Available' : 'Unavailable'}
+          </span>
         </div>
       </div>
     </div>
