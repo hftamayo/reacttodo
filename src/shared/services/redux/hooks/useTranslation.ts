d67ed4972533e663eb783a6e0b1ef0b9f2b2f67f
Herbert Tamayo <hftamayo@gmail.com>
2025-03-07 09:03:52 -0600

    updating useTranslation with settingsSlice

--- src/shared/services/redux/hooks/useTranslation.ts
+++ src/shared/services/redux/hooks/useTranslation.ts
@@ -1,5 +1,5 @@
 import { useAppSelector } from './useAppSelector';
-import { selectLanguage } from '../slices/languageSlice';
+import { selectLanguage } from '@/features/settings/store/settingsSlice';
 import { stringMessages } from '@/shared/utils/languages/stringMessages';
 import { LanguageContextType, Language } from '@/shared/types/settings.type';
 
