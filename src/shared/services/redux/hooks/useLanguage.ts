6a6dcd09430f7d620bb96c19718678cfb31665b7
Herbert Tamayo <hftamayo@gmail.com>
2025-03-07 09:03:38 -0600

    deprecating useLanguage, language and theme slicers

--- src/shared/services/redux/hooks/useLanguage.ts
+++
@@ -1,6 +0,0 @@
-import { useAppSelector } from './useAppSelector';
-import { selectLanguage } from '../slices/languageSlice';
-
-export const useLanguage = () => {
-  return useAppSelector(selectLanguage);
-};
