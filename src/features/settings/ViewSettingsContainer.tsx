b705e6acfefdea2befd81da2936e84d0578d7075
Herbert Tamayo <hftamayo@gmail.com>
2025-03-10 15:40:48 -0600

    first stage of viewSettings decoupling

--- src/features/settings/ViewSettingsContainer.tsx
+++ src/features/settings/ViewSettingsContainer.tsx
@@ -1,88 +1,26 @@
-import React, { useState } from 'react';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/shared/components/ui/card/Card';
-
-import { Input } from '@/shared/components/ui/input/Input';
-import { Label } from '@/shared/components/ui/label/Label';
-import {
-  RadioGroup,
-  RadioGroupItem,
-} from '@/shared/components/ui/rgroup/RadioGroup';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/shared/components/ui/select/Select';
-import { useSettings } from './hooks/useSettings';
+import React from 'react';
 import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
-import {
-  ViewSettingsFormProps,
-  Language,
-  Theme,
-} from '@/shared/types/settings.type';
-import { formSettingsStyles } from '@/shared/utils/twind/styles';
-import { showError } from '@/shared/services/notification/notificationService';
+import { ViewSettingsFormProps } from '@/shared/types/settings.type';
 import { SettingsFormActions } from './components/layout/SettingsFormActions';
 import { SettingsCard } from './components/layout/SettingsCard';
+import { useSettingsForm } from './hooks/useSettingsForm';
 
 export const ViewSettingsContainer: React.FC<ViewSettingsFormProps> = ({
   initialValues,
   onCancel,
   onSubmit,
 }) => {
-  const [formValues, setFormValues] = useState({
-    language: initialValues.language,
-    theme: initialValues.theme,
-    timezone: initialValues.timezone,
-    fontSize: initialValues.fontSize,
+  const [formValues, handlers] = useSettingsForm({
+    initialValues,
+    onCancel,
+    onSubmit,
   });
-
-  const { settings, updateSettings } = useSettings();
   const { title, text } = useTranslation('settingsForm');
   const { group } = useTranslation('settingsFormElements');
 
   if (!group) {
     return null;
   }
-
-  const handleLanguageChange = (value: Language) => {
-    setFormValues((prev) => ({ ...prev, language: value }));
-  };
-
-  const handleThemeChange = (value: Theme) => {
-    setFormValues((prev) => ({ ...prev, theme: value }));
-  };
-
-  const handleTimeZoneChange = (value: string) => {
-    setFormValues((prev) => ({ ...prev, timezone: value }));
-  };
-
-  const handleFontSizeChange = (value: string) => {
-    setFormValues((prev) => ({ ...prev, fontSize: Number(value) }));
-  };
-
-  const cancelHandler = () => {
-    onCancel();
-  };
-
-  const submitHandler = async (event: React.FormEvent) => {
-    event.preventDefault();
-    try {
-      updateSettings(formValues);
-      onSubmit(formValues);
-    } catch (error) {
-      showError('Failed to update settings');
-      //console.log(error);
-    }
-  };
 
   return (
     <SettingsCard title={title} description={text}>
