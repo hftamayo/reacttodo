810c96ea6bc08e60cb54308f14bcf07bae021506
Herbert Tamayo <hftamayo@gmail.com>
2025-03-10 14:58:57 -0600

    adding useSettingsForm hook

---
+++ src/features/settings/hooks/useSettingsForm.ts
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { useSettings } from './useSettings';
+import {
+  useSettingsFormProps,
+  AppSettings,
+  Language,
+  Theme,
+} from '@/shared/types/settings.type';
+import { showError } from '@/shared/services/notification/notificationService';
+
+export const useSettingsForm = ({
+  initialValues,
+  onSubmit,
+  onCancel,
+}: useSettingsFormProps) => {
+  const [formValues, setFormValues] = useState(initialValues);
+  const updateSettings = useSettings();
+
+  const handleLanguageChange = (value: Language) => {
+    setFormValues((prev) => ({ ...prev, language: value }));
+  };
+
+  const handleThemeChange = (value: Theme) => {
+    setFormValues((prev) => ({ ...prev, theme: value }));
+  };
+
+  const handleTimeZoneChange = (value: string) => {
+    setFormValues((prev) => ({ ...prev, timezone: value }));
+  };
+
+  const handleFontSizeChange = (value: string) => {
+    setFormValues((prev) => ({ ...prev, fontSize: Number(value) }));
+  };
+
+  const handleBackUpChange = (value: string) => {
+    setFormValues((prev) => ({ ...prev, backUp: value }));
+  };
+
+  const submitHandler = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      await updateSettings(formValues);
+      onSubmit(formValues);
+    } catch (error) {
+      showError('Failed to update settings');
+      //console.log(error);
+    }
+  };
+
+  const cancelHandler = () => {
+    setFormValues(initialValues);
+    onCancel();
+  };
+
+  return {
+    formValues,
+    handlers: {
+      handleLanguageChange,
+      handleThemeChange,
+      handleTimeZoneChange,
+      handleFontSizeChange,
+      handleBackUpChange,
+      submitHandler,
+      cancelHandler,
+    },
+  };
+};
