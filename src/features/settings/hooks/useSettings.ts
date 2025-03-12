fc9112bda374935598431d2472700324a52508fc
Herbert Tamayo <hftamayo@gmail.com>
2025-03-12 10:43:04 -0600

    calling updateSettings reducer

--- src/features/settings/hooks/useSettings.ts
+++ src/features/settings/hooks/useSettings.ts
@@ -1,24 +1,27 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { AppSettings } from '@/shared/types/settings.type';
 import {
-  setLanguage,
-  setTheme,
   updateSettings,
   selectSettings,
+  selectLanguage,
+  selectTheme,
 } from '../store/settingsSlice';
 import { AppDispatch } from '@/shared/services/redux/store';
 
 export const useSettings = () => {
   const dispatch = useDispatch<AppDispatch>();
   const settings = useSelector(selectSettings);
+  const language = useSelector(selectLanguage);
+  const theme = useSelector(selectTheme);
 
   return {
     settings,
-    setLanguage: (lang: AppSettings['language']) => dispatch(setLanguage(lang)),
-    setTheme: (theme: AppSettings['theme']) => dispatch(setTheme(theme)),
+    language,
+    theme,
     updateSettings: (newSettings: Partial<AppSettings>) =>
       dispatch(updateSettings(newSettings)),
   } as const;
 };
 
+// This type is useful for components that consume the hook
 export type UseSettingsReturn = ReturnType<typeof useSettings>;
