10058aa41020c50a426c1da706e722bdeaa29ad9
Herbert Tamayo <hftamayo@gmail.com>
2025-03-12 11:30:45 -0600

    first stable version of backup component

--- src/features/settings/components/sections/BackUp.tsx
+++ src/features/settings/components/sections/BackUp.tsx
@@ -1,6 +1,26 @@
-<div className="flex flex-col space-y-1.5">
-  <Label className={formSettingsStyles.grouptitle} htmlFor="txtbackup">
-    Backup and Restore
-  </Label>
-  <Input id="txtbackup" placeholder="todo" />
-</div>;
+import React, { useRef } from 'react';
+import { Label } from '@/shared/components/ui/label/Label';
+import { Input } from '@/shared/components/ui/input/Input';
+import { Button } from '@/shared/components/ui/button/Button';
+import { useSettings } from '../../hooks/useSettings';
+import { formSettingsStyles } from '@/shared/utils/twind/styles';
+import {
+  showSuccess,
+  showError,
+} from '@/shared/services/notification/notificationService';
+import { SettingsControlsProps } from '@/shared/types/settings.type';
+
+export const BackUp: React.FC<SettingsControlsProps<string>> = ({
+  value,
+  onChange,
+  labels,
+}) => {
+  return (
+    <div className="flex flex-col space-y-1.5">
+      <Label className={formSettingsStyles.grouptitle} htmlFor="txtbackup">
+        {labels.title}
+      </Label>
+      <Input id="txtbackup" placeholder="todo" />
+    </div>
+  );
+};
