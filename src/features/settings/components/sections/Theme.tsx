de9169bf812b265a665e8e70eeca5d5c37661124
Herbert Tamayo <hftamayo@gmail.com>
2025-03-13 08:40:13 -0600

    polishing Theme component

--- src/features/settings/components/sections/Theme.tsx
+++ src/features/settings/components/sections/Theme.tsx
@@ -1,13 +1,30 @@
-<div className="flex flex-col space-y-1.5">
-  <Label className={formSettingsStyles.grouptitle}>{group.lbltheme}</Label>
-  <RadioGroup value={formValues.theme} onValueChange={handleThemeChange}>
-    <div className="flex items-center space-x-2">
-      <RadioGroupItem value="dark" id="r1" />
-      <Label htmlFor="r1">{group.theme01}</Label>
+import React from 'react';
+import { Label } from '@/shared/components/ui/label/Label';
+import {
+  RadioGroup,
+  RadioGroupItem,
+} from '@/shared/components/ui/rgroup/RadioGroup';
+import { formSettingsStyles } from '@/shared/utils/twind/styles';
+import { SettingsControlsProps } from '@/shared/types/settings.type';
+
+export const Theme: React.FC<SettingsControlsProps<string>> = ({
+  value,
+  onChange,
+  labels,
+}) => {
+  return (
+    <div className="flex flex-col space-y-1.5">
+      <Label className={formSettingsStyles.grouptitle}>{group.lbltheme}</Label>
+      <RadioGroup value={formValues.theme} onValueChange={handleThemeChange}>
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem value="dark" id="r1" />
+          <Label htmlFor="r1">{group.theme01}</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem value="standard" id="r2" />
+          <Label htmlFor="r2">{group.theme02}</Label>
+        </div>
+      </RadioGroup>
     </div>
-    <div className="flex items-center space-x-2">
-      <RadioGroupItem value="standard" id="r2" />
-      <Label htmlFor="r2">{group.theme02}</Label>
-    </div>
-  </RadioGroup>
-</div>;
+  );
+};
