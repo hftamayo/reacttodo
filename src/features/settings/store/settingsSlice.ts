087d399974634bd32684311a2ece3a94477bca99
Herbert Tamayo <hftamayo@gmail.com>
2025-03-13 10:02:06 -0600

    adjusting settingsSlice with useSettings hook

--- src/features/settings/store/settingsSlice.ts
+++ src/features/settings/store/settingsSlice.ts
@@ -23,9 +23,10 @@ const settingsSlice = createSlice({
   },
 });
 
-export const selectSettings = (state: RootState) => state;
-export const selectLanguage = (state: RootState) => state.settings.language;
-export const selectTheme = (state: RootState) => state.settings.theme;
+export const selectSettings = (state: RootState) => state.settings.settings;
+export const selectLanguage = (state: RootState) =>
+  state.settings.settings.language;
+export const selectTheme = (state: RootState) => state.settings.settings.theme;
 
 export const { updateSettings } = settingsSlice.actions;
 export default settingsSlice.reducer;
