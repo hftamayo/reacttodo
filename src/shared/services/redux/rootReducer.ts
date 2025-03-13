4e4ed19d651ae4a87809eceb37e2cecd26f8bf4d
Herbert Tamayo <hftamayo@gmail.com>
2025-03-13 10:01:44 -0600

    updating rootReducer with the use of useSettings hook

--- src/shared/services/redux/rootReducer.ts
+++ src/shared/services/redux/rootReducer.ts
@@ -1,13 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import taskReducer from '../../../features/task/store/taskSlice';
-import themeReducer from './slices/themeSlice';
-import languageReducer from './slices/languageSlice';
+import settingsReducer from '../../../features/settings/store/settingsSlice';
 import healthMetricsReducer from '../../../features/healthcheck/store/healthMetricsSlice';
 
 export const rootReducer = combineReducers({
   task: taskReducer,
-  theme: themeReducer,
-  language: languageReducer,
+  settings: settingsReducer,
   healthMetrics: healthMetricsReducer,
 });
 
