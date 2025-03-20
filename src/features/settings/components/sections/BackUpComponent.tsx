30d500c8816f0fe01173e450f0f66a8c14e058e9
Herbert Tamayo <hftamayo@gmail.com>
2025-03-20 08:50:43 -0600

    fixing BackUpComponent props

--- src/features/settings/components/sections/BackUpComponent.tsx
+++ src/features/settings/components/sections/BackUpComponent.tsx
@@ -10,9 +10,9 @@ import {
 } from '@/shared/services/notification/notificationService';
 import { SettingsFormProps } from '@/shared/types/settings.type';
 
-export const BackUpComponent: React.FC<
-  Omit<SettingsFormProps, 'onCancel, onSubmit'>
-> = ({ labels }) => {
+type BackUpProps = Pick<SettingsFormProps, 'labels'>;
+
+export const BackUpComponent: React.FC<BackUpProps> = ({ labels }) => {
   return (
     <div className="flex flex-col space-y-1.5">
       <Label className={formSettingsStyles.grouptitle} htmlFor="txtbackup">
