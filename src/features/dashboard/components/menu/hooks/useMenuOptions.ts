7c936df2ae1e626991f3b75078af3e14bf0c75d2
Herbert Tamayo <hftamayo@gmail.com>
2025-03-25 09:40:23 -0600

    update on menuOptions hook

--- src/features/dashboard/components/menu/hooks/useMenuOptions.ts
+++ src/features/dashboard/components/menu/hooks/useMenuOptions.ts
@@ -1,59 +1,18 @@
-import {
-  FaHome,
-  FaRegEnvelope,
-  FaRegFileAlt,
-  FaPoll,
-  FaCog,
-} from 'react-icons/fa';
 import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
+import { MENU_ITEMS } from '../constants/menuItems';
 
-export const menuOptions = () => {
+export const useMenuOptions = (userRole: string) => {
   const { group } = useTranslation('sideBarDashboard');
 
   if (!group) {
-    return {
-      adminMenuOptions: [],
-      userMenuOptions: [],
-      supervisorMenuOptions: [],
-    };
+    return [];
   }
 
-  return {
-    adminMenuOptions: [
-      { path: '/', label: group.home, icon: FaHome },
-      {
-        path: '/dashboard/entities/roles',
-        label: group.roles,
-        icon: FaRegFileAlt,
-      },
-      {
-        path: '/dashboard/entities/users',
-        label: group.users,
-        icon: FaRegFileAlt,
-      },
-      { path: '/taskboard', label: group.tasks, icon: FaRegFileAlt },
-      { path: '/dashboard/inbox', label: group.inbox, icon: FaRegEnvelope },
-      { path: '/dashboard/settings', label: group.settings, icon: FaCog },
-      { path: '/dashboard/reports', label: group.reports, icon: FaPoll },
-    ],
-
-    userMenuOptions: [
-      { path: '/', label: group.home, icon: FaHome },
-      { path: '/taskboard', label: group.tasks, icon: FaRegFileAlt },
-      { path: '/dashboard/inbox', label: group.inbox, icon: FaRegEnvelope },
-      { path: '/dashboard/reports', label: group.reports, icon: FaPoll },
-    ],
-
-    supervisorMenuOptions: [
-      { path: '/', label: group.home, icon: FaHome },
-      {
-        path: '/dashboard/entities/users',
-        label: group.users,
-        icon: FaRegFileAlt,
-      },
-      { path: '/taskboard', label: group.tasks, icon: FaRegFileAlt },
-      { path: '/dashboard/inbox', label: group.inbox, icon: FaRegEnvelope },
-      { path: '/dashboard/reports', label: group.reports, icon: FaPoll },
-    ],
-  };
+  return MENU_ITEMS.filter((item) => item.roles.includes(userRole)).map(
+    (item) => ({
+      path: item.path,
+      label: group[item.label],
+      icon: item.icon,
+    })
+  );
 };
