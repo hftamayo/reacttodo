a2fba37e7dab124a9b21d21a71afe872f0151e51
Herbert Tamayo <hftamayo@gmail.com>
2025-03-25 09:53:20 -0600

    refactoring menuItem and contMenuBar part1

--- src/features/dashboard/components/menu/components/MenuItem.tsx
+++ src/features/dashboard/components/menu/components/MenuItem.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'wouter';
-import { MenuOptionProps } from '@/shared/types/menu.type';
+import { MenuItemProps } from '@/shared/types/menu.type';
 import { DashBoardMenuBarStyles } from '@/shared/utils/twind/styles';
 
-export const MenuItem: React.FC<MenuOptionProps> = ({ option }) => {
+export const MenuItem: React.FC<MenuItemProps> = ({ option }) => {
   return (
     <li className={DashBoardMenuBarStyles.listItem}>
       <Link to={option.path} className={DashBoardMenuBarStyles.linkItem}>
