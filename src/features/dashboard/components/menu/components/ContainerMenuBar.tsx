566fa7d0a81c81a6a6b2ef44ec13a8d0837ea0df
Herbert Tamayo <hftamayo@gmail.com>
2025-03-25 10:30:52 -0600

    implementing dropDownMenuItem component

--- src/features/dashboard/components/menu/components/ContainerMenuBar.tsx
+++ src/features/dashboard/components/menu/components/ContainerMenuBar.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import { DropDownMenu } from './DropDownMenu';
+import { DropDownMenuItem } from './DropDownMenuItem';
 import { MenuItem } from './MenuItem';
 import { MenuProps } from '@/shared/types/menu.type';
 
-export const ContainerMenuBar: React.FC<MenuProps> = ({ options }) => {
+export const ContainerMenuBar: React.FC<MenuProps> = (userRole: string) => {
+  const userRole = 'admin'; // Get this from your auth context
   return (
     <div className="w-64 text-white p-4">
       <ul className="space-y-2">
-        {options.map((option) => (
-          <MenuItem key={option.path} option={option} />
+        {menuOptions.map((option) => (
+          <MenuItem key={option.path} {...option} />
         ))}
       </ul>
       <div className="mt-4">
         <ul>
-          <DropDownMenu options={options} />
+          <DropDownMenuItem
+            userRole={userRole}
+            managementOptions={['roles', 'users', 'tasks']}
+          />
         </ul>
       </div>
     </div>
