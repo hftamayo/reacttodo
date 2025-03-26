219be3ba31869b8bee8f4040637e019f712c909e
Herbert Tamayo <hftamayo@gmail.com>
2025-03-26 09:46:42 -0600

    updating container menu for better menu management

--- src/features/dashboard/components/menu/components/ContainerMenuBar.tsx
+++ src/features/dashboard/components/menu/components/ContainerMenuBar.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import { DropDownMenuItem } from './DropDownMenuItem';
 import { MenuItem } from './MenuItem';
-import { MenuProps } from '@/shared/types/menu.type';
+import { DropDownMenuItemProps } from '@/shared/types/menu.type';
+import { useMenuOptions } from '../hooks/useMenuOptions';
 
-export const ContainerMenuBar: React.FC<MenuProps> = (userRole: string) => {
-  const userRole = 'admin'; // Get this from your auth context
+export const ContainerMenuBar: React.FC<DropDownMenuItemProps> = ({
+  userRole,
+  managementOptions,
+}) => {
+  const menuOptions = useMenuOptions(userRole);
+
+  const mainMenuItems = menuOptions.filter(
+    (option) => !managementOptions.includes(option.label.toLowerCase())
+  );
+
   return (
     <div className="w-64 text-white p-4">
       <ul className="space-y-2">
-        {menuOptions.map((option) => (
-          <MenuItem key={option.path} {...option} />
+        {mainMenuItems.map((option) => (
+          <MenuItem key={option.path} option={option} userRole={userRole} />
         ))}
       </ul>
       <div className="mt-4">
         <ul>
           <DropDownMenuItem
             userRole={userRole}
-            managementOptions={['roles', 'users', 'tasks']}
+            managementOptions={managementOptions}
           />
         </ul>
       </div>
