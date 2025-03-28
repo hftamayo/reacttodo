bd016e53a65b226b6c81021f78d6e7606132a051
Herbert Tamayo <hftamayo@gmail.com>
2025-03-28 09:53:59 -0600

    updating menuService with more methods

--- src/features/dashboard/components/menu/services/menuService.ts
+++ src/features/dashboard/components/menu/services/menuService.ts
@@ -1,14 +1,18 @@
 import { MenuItemProps } from '@/shared/types/menu.type';
 
 export const menuService = {
-  hasAccess: (item: MenuItemProps, userRole: string): boolean => {
-    return item.roles.includes(userRole);
+  filterItemsByRole: (
+    items: MenuItemProps[],
+    userRole: string
+  ): MenuItemProps[] => {
+    return items.filter((item) => item.roles.includes(userRole));
   },
 
-  getMenuItemsByRole: (
-    items: MenuItemProps[],
-    role: string
-  ): MenuItemProps[] => {
-    return items.filter((item) => item.roles.includes(role));
+  getDropdownItems: (items: MenuItemProps[]): MenuItemProps[] => {
+    return items.filter((item) => item.isDropDownItem);
+  },
+
+  getMainMenuItems: (items: MenuItemProps[]): MenuItemProps[] => {
+    return items.filter((item) => !item.isDropDownItem);
   },
 };
