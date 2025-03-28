d4dc3c057e1228c05203228b2267fb180a84f9c3
Herbert Tamayo <hftamayo@gmail.com>
2025-03-28 10:34:35 -0600

    updating menu hook

--- src/features/dashboard/components/menu/hooks/useMenuOptions.ts
+++ src/features/dashboard/components/menu/hooks/useMenuOptions.ts
@@ -1,20 +1,36 @@
+import { useAppSelector } from '@/shared/services/redux/hooks/useAppSelector';
 import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
-import { MENU_ITEMS } from '../constants/menuItems';
-import { MenuItemProps } from '@/shared/types/menu.type';
+import { selectDropDownItems, selectMainMenuItems } from '../store/menuSlice';
+import { menuService } from '../services/menuService';
 
-export const useMenuOptions = (userRole: string): MenuItemProps[] => {
+export const useMenuOptions = (userRole: string) => {
+  const dropDownItems = useAppSelector(selectDropDownItems);
+  const mainMenuItems = useAppSelector(selectMainMenuItems);
   const { group } = useTranslation('sideBarDashboard');
 
   if (!group) {
-    return [];
+    return {
+      dropdownItems: [],
+      mainMenuItems: [],
+    };
   }
 
-  return MENU_ITEMS.filter((item) => item.roles.includes(userRole)).map(
-    (item) => ({
-      path: item.path,
-      label: group[item.label],
-      icon: item.icon,
-      roles: item.roles,
-    })
-  );
+  const translatedDropdownItems = menuService
+    .filterItemsByRole(dropDownItems, userRole)
+    .map((item) => ({
+      ...item,
+      label: group[item.label] || item.label,
+    }));
+
+  const translatedMainItems = menuService
+    .filterItemsByRole(mainMenuItems, userRole)
+    .map((item) => ({
+      ...item,
+      label: group[item.label] || item.label,
+    }));
+
+  return {
+    dropdownItems: translatedDropdownItems,
+    mainMenuItems: translatedMainItems,
+  };
 };
