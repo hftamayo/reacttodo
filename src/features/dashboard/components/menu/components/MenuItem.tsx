2f6e1bb654eb2216059220b47a6058d23983b0b9
Herbert Tamayo <hftamayo@gmail.com>
2025-03-28 10:06:49 -0600

    adjusting menuItem component with new store model

--- src/features/dashboard/components/menu/components/MenuItem.tsx
+++ src/features/dashboard/components/menu/components/MenuItem.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { Link } from 'wouter';
 import { DashBoardMenuBarStyles } from '@/shared/utils/twind/styles';
 import { MenuLinkProps } from '@/shared/types/menu.type';
-import { menuService } from '../services/menuService';
 
 export const MenuItem: React.FC<MenuLinkProps> = ({ option, userRole }) => {
-  if (!menuService.hasAccess(option, userRole)) {
+  if (option.isDropDownItem) {
     return null;
   }
-
   return (
     <li className={DashBoardMenuBarStyles.listItem}>
       <Link to={option.path} className={DashBoardMenuBarStyles.linkItem}>
