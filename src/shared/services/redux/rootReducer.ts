af98fe32fcfd656f8267c49e7eaba2649b14ab95
Herbert Tamayo <hftamayo@gmail.com>
2025-03-28 10:24:27 -0600

    updating rootReducer with menuSlicer

--- src/shared/services/redux/rootReducer.ts
+++ src/shared/services/redux/rootReducer.ts
@@ -2,11 +2,13 @@ import { combineReducers } from '@reduxjs/toolkit';
 import taskReducer from '../../../features/task/store/taskSlice';
 import settingsReducer from '../../../features/settings/store/settingsSlice';
 import healthMetricsReducer from '../../../features/healthcheck/store/healthMetricsSlice';
+import menuReducer from '../../../features/dashboard/components/menu/store/menuSlice';
 
 export const rootReducer = combineReducers({
   task: taskReducer,
   settings: settingsReducer,
   healthMetrics: healthMetricsReducer,
+  menu: menuReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
