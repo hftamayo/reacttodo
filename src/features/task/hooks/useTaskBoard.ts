46c9e4784da5bcb1c79f413c7906dc795cd0c020
Herbert Tamayo <hftamayo@gmail.com>
2025-04-09 12:51:35 -0600

    updating useTaskBoard with changes on 2 more hooks related

--- src/features/task/hooks/useTaskBoard.ts
+++ src/features/task/hooks/useTaskBoard.ts
@@ -2,13 +2,15 @@ import { useEffect, useMemo } from 'react';
 import { useLazyLoad } from '@/shared/services/lazyloading/hooks/useLazyLoad';
 import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
 import { showError } from '@/shared/services/notification/notificationService';
-import { taskHooks } from './taskHooks';
+import { useTaskQueries } from './useTaskQueries';
+import { useTaskMutations } from './useTaskMutations';
 
 export const useTaskBoard = () => {
   const { ref, shouldFetch } = useLazyLoad();
-  const { data, error, isLoading } = taskHooks.useGetTasks(shouldFetch);
+  const { data, error, isLoading } = useTaskQueries.getTasks(shouldFetch);
   const { text: errorMessage = 'An error occurred' } =
     useTranslation('errorComponent');
+  const mutations = useTaskMutations();
 
   const tasks = useMemo(() => {
     return data?.data.tasks ? Array.from(data.data.tasks.values()) : [];
@@ -35,5 +37,6 @@ export const useTaskBoard = () => {
     isLoading,
     errorMessage,
     taskStats,
+    mutations,
   };
 };
