6324dafd932d895408fdcc0e0496af75bdf08018
Herbert Tamayo <hftamayo@gmail.com>
2025-04-09 11:55:43 -0600

    cleanin up Taskboard Container

--- src/containers/TaskBoard/TaskBoardContainer.tsx
+++ src/containers/TaskBoard/TaskBoardContainer.tsx
@@ -1,33 +1,18 @@
-import React, { useEffect } from 'react';
-import { taskHooks } from '@/features/task/hooks/taskHooks';
+import { useMemo } from 'react';
+import { useTaskBoard } from '@/features/task/hooks/useTaskBoard';
 import TaskBoardPresenter from './TaskBoardPresenter';
-import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
-import { useLazyLoad } from '@/shared/services/lazyloading/hooks/useLazyLoad';
-import { showError } from '@/shared/services/notification/notificationService';
 
 export const TaskBoardContainer: React.FC = () => {
-  const { ref, shouldFetch } = useLazyLoad();
-  const { data, error, isLoading } = taskHooks.useGetTasks(shouldFetch);
-  const tasks = data?.data.tasks ? Array.from(data.data.tasks.values()) : [];
-  const { text: errorComponent = 'An error occurred' } =
-    useTranslation('errorComponent');
-
-  const totalTasks = tasks.length;
-  const completedTasks = tasks.filter((task) => task.done).length;
-
-  useEffect(() => {
-    if (error) {
-      showError('unexpected UI error', errorComponent);
-    }
-  }, [error, errorComponent]);
+  const { ref, tasks, error, isLoading, errorMessage, taskStats } =
+    useTaskBoard();
 
   return (
     <TaskBoardPresenter
       ref={ref}
       tasks={tasks}
       isLoading={isLoading}
-      totalTasks={totalTasks}
-      completedTasks={completedTasks}
+      totalTasks={taskStats.total}
+      completedTasks={taskStats.completed}
       error={error ?? undefined}
     />
   );
