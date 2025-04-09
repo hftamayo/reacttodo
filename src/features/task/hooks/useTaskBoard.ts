f813424b0f722226f2d65cc5063a0bf1ca559f36
Herbert Tamayo <hftamayo@gmail.com>
2025-04-09 11:52:25 -0600

    adding a hook for TaskBoard

---
+++ src/features/task/hooks/useTaskBoard.ts
@@ -0,0 +1,39 @@
+import { useEffect, useMemo } from 'react';
+import { useLazyLoad } from '@/shared/services/lazyloading/hooks/useLazyLoad';
+import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
+import { showError } from '@/shared/services/notification/notificationService';
+import { taskHooks } from './taskHooks';
+
+export const useTaskBoard = () => {
+  const { ref, shouldFetch } = useLazyLoad();
+  const { data, error, isLoading } = taskHooks.useGetTasks(shouldFetch);
+  const { text: errorMessage = 'An error occurred' } =
+    useTranslation('errorComponent');
+
+  const tasks = useMemo(() => {
+    return data?.data.tasks ? Array.from(data.data.tasks.values()) : [];
+  }, [data]);
+
+  const taskStats = useMemo(
+    () => ({
+      total: tasks.length,
+      completed: tasks.filter((task) => task.done).length,
+    }),
+    [tasks]
+  );
+
+  useEffect(() => {
+    if (error) {
+      showError('unexpected UI error', errorMessage);
+    }
+  }, [error, errorMessage]);
+
+  return {
+    ref,
+    tasks,
+    error,
+    isLoading,
+    errorMessage,
+    taskStats,
+  };
+};
