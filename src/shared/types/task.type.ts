68b692ff99d018fceb9acf0ea40ac690d265d74b
Herbert Tamayo <hftamayo@gmail.com>
2025-05-09 10:08:02 -0600

    adjusting types related to update task

--- src/shared/types/task.type.ts
+++ src/shared/types/task.type.ts
@@ -11,13 +11,22 @@ export type FullTask = {
 export type TaskProps = Omit<FullTask, 'createdAt' | 'updatedAt'>;
 
 export type TaskCardProps = {
+  id: number;
   title: string;
   description: string;
   done: boolean;
+  owner: number;
   onClose?: () => void;
 };
 
 export type TaskCardFormProps = {
+  initialData: {
+    id: number;
+    title: string;
+    description: string;
+    done: boolean;
+    owner: number;
+  };
   onCancel: () => void;
   onSubmit: (values: TaskProps) => void;
 };
