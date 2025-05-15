5e6f3ed9c28191e89fafdea91ab22f92c71877a7
Herbert Tamayo <hftamayo@gmail.com>
2025-05-15 13:51:23 -0600

    updating useTaskBoard with pagination support

--- src/features/task/hooks/useTaskBoard.ts
+++ src/features/task/hooks/useTaskBoard.ts
@@ -4,47 +4,54 @@ import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
 import { showError } from '@/shared/services/notification/notificationService';
 import { useTaskQueries } from './useTaskQueries';
 import { useTaskMutations } from './useTaskMutations';
+import { PaginationParams, TaskBoardState } from '@/shared/types/api.type';
 
-export const useTaskBoard = () => {
+export const useTaskBoard = ({
+  currentPage,
+  limit,
+}: PaginationParams): TaskBoardState => {
   const { ref, shouldFetch } = useLazyLoad();
-  const { data, error, isLoading } = useTaskQueries.getTasks(shouldFetch);
+  const { data, error, isLoading } = useTaskQueries.getTasks(
+    shouldFetch,
+    currentPage,
+    limit
+  );
   const { text: errorMessage = 'An error occurred' } =
     useTranslation('errorComponent');
   const mutations = useTaskMutations();
 
   const tasks = useMemo(() => {
-    if (isLoading) {
-      return [];
-    }
-
-    if (!data?.data?.tasks) {
-      console.error('No tasks found in the API response');
-      return [];
-    }
+    if (isLoading || !data?.data?.tasks) return [];
     return data.data.tasks;
   }, [data, isLoading]);
 
-  const taskStats = useMemo(
+  const { pagination, stats } = useMemo(
     () => ({
-      total: data?.data?.pagination.totalCount ?? 0,
-      completed: tasks.filter((task) => task.done).length,
+      pagination: {
+        currentPage: data?.data?.pagination?.currentPage ?? currentPage,
+        totalPages: data?.data?.pagination?.totalPages ?? 1,
+      },
+      stats: {
+        total: data?.data?.pagination?.totalCount ?? 0,
+        completed: tasks.filter((task) => task.done).length,
+      },
     }),
-    [data, tasks]
+    [data, tasks, currentPage]
   );
 
   useEffect(() => {
     if (error) {
       showError('unexpected UI error', errorMessage);
     }
-  }, [error, errorMessage]);
+  }, [error]);
 
   return {
     ref,
     tasks,
-    error,
     isLoading,
-    errorMessage,
-    taskStats,
+    error,
+    pagination,
+    taskStats: stats,
     mutations,
   };
 };
