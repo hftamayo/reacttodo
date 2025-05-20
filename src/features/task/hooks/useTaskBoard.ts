ffc8e8cd7c58307ba0aeb585037b6f889f230a6a
Herbert Tamayo <hftamayo@gmail.com>
2025-05-20 13:11:14 -0600

    updating useTaskBoard with offset based pagination

--- src/features/task/hooks/useTaskBoard.ts
+++ src/features/task/hooks/useTaskBoard.ts
@@ -1,26 +1,20 @@
-import { useEffect, useMemo } from 'react';
+import { useMemo } from 'react';
 import { useLazyLoad } from '@/shared/services/lazyloading/hooks/useLazyLoad';
 import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
 import { showError } from '@/shared/services/notification/notificationService';
 import { useTaskQueries } from './useTaskQueries';
 import { useTaskMutations } from './useTaskMutations';
-import {
-  CursorParams,
-  CursorPagination,
-  TaskBoardState,
-  PaginationMetadata,
-} from '@/shared/types/api.type';
+import { PaginationParams, TaskBoardState } from '@/shared/types/api.type';
 
 export const useTaskBoard = ({
+  page,
   limit,
-  cursor,
-}: CursorParams): TaskBoardState => {
+}: PaginationParams): TaskBoardState => {
   const { ref, shouldFetch } = useLazyLoad();
   const { data, error, isLoading } = useTaskQueries.getTasks({
     enabled: shouldFetch,
-    paginationType: 'cursor',
+    page,
     limit,
-    cursor,
   });
   const { text: errorMessage = 'An error occurred' } =
     useTranslation('errorComponent');
@@ -32,20 +26,16 @@ export const useTaskBoard = ({
   }, [data, isLoading]);
 
   const { pagination, stats } = useMemo(() => {
-    const paginationData = data?.data?.pagination as CursorPagination;
+    const paginationData = data?.data?.pagination;
 
     return {
       pagination: {
-        type: 'cursor',
         limit: paginationData?.limit ?? 5,
         totalCount: paginationData?.totalCount ?? 0,
-        hasMore: paginationData?.hasMore ?? false,
         currentPage: paginationData?.currentPage ?? 1,
         totalPages: paginationData?.totalPages ?? 1,
         order: paginationData?.order ?? 'desc',
-        nextCursor: paginationData?.nextCursor ?? null,
-        prevCursor: paginationData?.prevCursor ?? null,
-      } as CursorPagination,
+      },
       stats: {
         total: paginationData?.totalCount ?? 0,
         completed: tasks.filter((task) => task.done).length,
@@ -54,7 +44,6 @@ export const useTaskBoard = ({
   }, [data, tasks]);
 
   return {
-    ref,
     tasks,
     isLoading,
     error,
