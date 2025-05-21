79458bfb81e6ef1559b5ca424133fb9c9138e2d7
Herbert Tamayo <hftamayo@gmail.com>
2025-05-21 09:44:00 -0600

    types for selector during ui operations

---
+++ src/shared/services/redux/types.ts
@@ -0,0 +1,14 @@
+import { RootState } from './rootReducer';
+
+// Extract state types from RootState
+export type TaskUIState = RootState['taskUI'];
+export type TaskState = RootState['task'];
+export type SettingsState = RootState['settings'];
+export type HealthMetricsState = RootState['healthMetrics'];
+export type MenuState = RootState['menu'];
+
+// Type for the last operation in TaskUI
+export type TaskOperationType = TaskUIState['lastOperation']['type'];
+
+// Type for optimistic updates
+export type OptimisticUpdates = TaskUIState['optimisticUpdates']; 
