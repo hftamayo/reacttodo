68844ae50f766dc24d44d24bd48d269ff90cf8e9
Herbert Tamayo <hftamayo@gmail.com>
2025-05-21 09:46:54 -0600

    adding selectors and redux logger during development

--- src/shared/services/redux/store.ts
+++ src/shared/services/redux/store.ts
@@ -1,9 +1,16 @@
+// src/shared/services/redux/store.ts
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './rootReducer';
+import logger from 'redux-logger'; // for development
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  middleware: (getDefaultMiddleware) => {
+    const middlewares = getDefaultMiddleware();
+    if (process.env.NODE_ENV === 'development') {
+      middlewares.push(logger);
+    }
+    return middlewares;
+  },
+  devTools: process.env.NODE_ENV !== 'production',
 });
-
-export type AppDispatch = typeof store.dispatch;
