6e317c7059bd5f9d3c99ee7c86c7a561229c412c
Herbert Tamayo <hftamayo@gmail.com>
2025-05-26 13:52:40 -0600

    cleaning up taskSelectors

--- src/features/task/store/taskSelectors.ts
+++ src/features/task/store/taskSelectors.ts
@@ -1,19 +1,23 @@
 import { RootState } from '@/shared/services/redux/rootReducer';
-import { TaskUIState, TaskOperationType } from '@/shared/services/redux/types';
+import { TaskUIState } from '@/shared/services/redux/types';
 import { createSelector } from '@reduxjs/toolkit';
 
 // Basic selectors
 export const selectTaskUI = (state: RootState): TaskUIState => state.taskUI;
-export const selectOptimisticUpdates = (state: RootState) => state.taskUI.optimisticUpdates;
-export const selectLastOperation = (state: RootState) => state.taskUI.lastOperation;
+export const selectLastOperation = (state: RootState) =>
+  state.taskUI.lastOperation;
 export const selectViewMode = (state: RootState) => state.taskUI.viewMode;
-export const selectSelectedTaskId = (state: RootState) => state.taskUI.selectedTaskId;
+export const selectSelectedTaskId = (state: RootState) =>
+  state.taskUI.selectedTaskId;
 
 // Memoized selectors using createSelector
 export const selectIsTaskOptimistic = (taskId: number) =>
   createSelector(
-    [selectOptimisticUpdates],
-    (optimisticUpdates) => !!optimisticUpdates[taskId]
+    [selectLastOperation],
+    (lastOperation) =>
+      lastOperation.taskId === taskId &&
+      !!lastOperation.timestamp &&
+      Date.now() - lastOperation.timestamp < 2000 // 2 second threshold
   );
 
 export const selectLastOperationType = createSelector(
@@ -29,10 +33,3 @@ export const selectIsOperationInProgress = createSelector(
     return now - lastOperation.timestamp < 2000; // 2 seconds threshold
   }
 );
-
-// Selector to get task with optimistic updates
-export const selectTaskWithOptimisticUpdates = (taskId: number) =>
-  createSelector(
-    [selectOptimisticUpdates],
-    (optimisticUpdates) => optimisticUpdates[taskId]
-  ); 
