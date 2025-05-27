4289aad85c9c001d388ba0305e84608d28562f5f
Herbert Tamayo <hftamayo@gmail.com>
2025-05-27 15:56:57 -0600

    updating rootReducer

--- src/shared/services/redux/rootReducer.ts
+++ src/shared/services/redux/rootReducer.ts
@@ -1,13 +1,13 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import taskReducer from '../../../features/task/store/taskSlice';
 import taskUIReducer from '../../../features/task/store/taskSlice';
+import paginationReducer from '../../../features/task/store/paginationSlice';
 import settingsReducer from '../../../features/settings/store/settingsSlice';
 import healthMetricsReducer from '../../../features/healthcheck/store/healthMetricsSlice';
 import menuReducer from '../../../features/dashboard/components/menu/store/menuSlice';
 
 export const rootReducer = combineReducers({
-  task: taskReducer,
   taskUI: taskUIReducer,
+  pagination: paginationReducer,
   settings: settingsReducer,
   healthMetrics: healthMetricsReducer,
   menu: menuReducer,
