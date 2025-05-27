27c34041066e17fcb25b87f313968389f3f5f4c6
Herbert Tamayo <hftamayo@gmail.com>
2025-05-27 14:03:21 -0600

    improving code of react query client oriented to cache handling

--- src/shared/services/cache/queryClient.ts
+++ src/shared/services/cache/queryClient.ts
@@ -3,13 +3,10 @@ import { QueryClient } from '@tanstack/react-query';
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1000 * 60 * 5, // 5 minutes
-      gcTime: 1000 * 60 * 10, // 10 minutes
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      gcTime: 10 * 60 * 1000, // 10 minutes
       retry: 1,
       refetchOnWindowFocus: false,
-    },
-    mutations: {
-      retry: 1,
     },
   },
 });
