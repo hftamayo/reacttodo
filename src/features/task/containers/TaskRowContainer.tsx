8c2b1e39c39a243c6f041f911a1633157f989b72
Herbert Tamayo <hftamayo@gmail.com>
2025-05-28 12:16:05 -0600

    cleaning up taskRowContainer

--- src/features/task/containers/TaskRowContainer.tsx
+++ src/features/task/containers/TaskRowContainer.tsx
@@ -1,33 +1,28 @@
 import React from 'react';
 import { TaskProps } from '@/shared/types/api.type';
 import { TaskRow } from '../components/TaskRow';
-import { useTaskOperations } from '../hooks/useTaskOperations';
-import { useDispatch } from 'react-redux';
-import { setLastOperation } from '../store/taskSlice';
 import { showError } from '@/shared/services/notification/notificationService';
 
 interface TaskRowContainerProps {
   task: TaskProps;
   onEdit: (task: TaskProps) => void;
+  onToggleTask: (id: number) => Promise<void>;
+  onDeleteTask: (id: number) => Promise<void>;
+  isToggling?: boolean;
+  isDeleting?: boolean;
 }
 
-export const TaskRowContainer: React.FC<TaskRowContainerProps> = ({ task, onEdit }) => {
-  const dispatch = useDispatch();
-  const { handleToggle, handleDelete, isToggling, isDeleting } = useTaskOperations();
-
+export const TaskRowContainer: React.FC<TaskRowContainerProps> = ({
+  task,
+  onEdit,
+  onToggleTask,
+  onDeleteTask,
+  isToggling = false,
+  isDeleting = false,
+}) => {
   const handleToggleComplete = async () => {
     try {
-      // Set optimistic update in Redux
-      dispatch(setLastOperation({ type: 'toggle', taskId: task.id }));
-      
-      // Perform the toggle operation
-      const success = await handleToggle(task.id);
-      
-      if (!success) {
-        // If the operation failed, we don't need to do anything
-        // as the optimistic update will be rolled back
-        return;
-      }
+      await onToggleTask(task.id);
     } catch (error) {
       console.error('Error toggling task:', error);
       showError('Failed to toggle task completion');
@@ -36,9 +31,7 @@ export const TaskRowContainer: React.FC<TaskRowContainerProps> = ({ task, onEdit
 
   const handleDeleteTask = async () => {
     try {
-      // Set optimistic update in Redux
-      dispatch(setLastOperation({ type: 'delete', taskId: task.id }));
-      await handleDelete(task.id);
+      await onDeleteTask(task.id);
     } catch (error) {
       console.error('Error deleting task:', error);
       showError('Failed to delete task');
@@ -57,4 +50,4 @@ export const TaskRowContainer: React.FC<TaskRowContainerProps> = ({ task, onEdit
   );
 };
 
-TaskRowContainer.displayName = 'TaskRowContainer'; 
+TaskRowContainer.displayName = 'TaskRowContainer';
