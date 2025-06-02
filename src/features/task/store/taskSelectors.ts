96120367f121f0137a5cfd09f957b66849747d63
Herbert Tamayo <hftamayo@gmail.com>
2025-06-02 10:04:37 -0600

    cleaning up taskSelector code

--- src/features/task/store/taskSelectors.ts
+++ src/features/task/store/taskSelectors.ts
@@ -1,10 +1,8 @@
 import { RootState } from '@/shared/services/redux/rootReducer';
 import { TaskUIState } from '@/shared/services/redux/types';
-import { createSelector } from '@reduxjs/toolkit';
 
 // Basic selectors
 export const selectTaskUI = (state: RootState): TaskUIState => state.taskUI;
 export const selectViewMode = (state: RootState) => state.taskUI.viewMode;
 export const selectSelectedTaskId = (state: RootState) =>
   state.taskUI.selectedTaskId;
-
