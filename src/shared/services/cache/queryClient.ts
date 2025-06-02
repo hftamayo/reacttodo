b706df8429d34d0770015dcdb9dc1c8512958fbd
Herbert Tamayo <hftamayo@gmail.com>
2025-06-02 12:46:56 -0600

    reduce stale time for data refetching

--- src/shared/services/cache/queryClient.ts
+++ src/shared/services/cache/queryClient.ts
@@ -3,7 +3,7 @@ import { QueryClient } from '@tanstack/react-query';
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 5 * 60 * 1000, // 5 minutes
+      staleTime: 0, // force data refetch
       gcTime: 10 * 60 * 1000, // 10 minutes
       retry: 1,
       refetchOnWindowFocus: false,
