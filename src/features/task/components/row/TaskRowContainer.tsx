e4145cd673be1e70be9207ace05b1ed28ec446a0
Herbert Tamayo <hftamayo@gmail.com>
2025-07-09 08:02:09 -0600

    removing deprecated import

--- src/features/task/components/row/TaskRowContainer.tsx
+++ src/features/task/components/row/TaskRowContainer.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { TaskProps } from '@/shared/types/domains/task.type';
 import { TaskRow } from './TaskRow';
-import { PaginationParams } from '@/shared/types/api.type';
 
 interface TaskRowContainerProps {
   task: TaskProps;
 }
 
-export const TaskRowContainer: React.FC<TaskRowContainerProps> = ({
-  task,
-}) => {
+export const TaskRowContainer: React.FC<TaskRowContainerProps> = ({ task }) => {
   return <TaskRow {...task} />;
 };
 
