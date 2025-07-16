7cb3742ca71bc4b637546229c22ee923461264fe
Herbert Tamayo <hftamayo@gmail.com>
2025-07-16 15:44:38 -0600

    first approach of LoginCard

---
+++ src/features/auth/components/login/LoginCard.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+} from '@/shared/components/ui/card/Card';
+import { formStyles } from '@/shared/utils/twind/styles';
+import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
