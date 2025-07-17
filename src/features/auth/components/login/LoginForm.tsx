15486bdde0b2f389a8202a5fa38b60ae96f94915
Herbert Tamayo <hftamayo@gmail.com>
2025-07-17 15:37:27 -0600

    first set of Loginform elements

---
+++ src/features/auth/components/login/LoginForm.tsx
@@ -0,0 +1,54 @@
+import { FC } from 'react';
+import { Label } from '@/shared/components/ui/label/Label';
+import { Input } from '@/shared/components/ui/input/Input';
+import { Checkbox } from '@/shared/components/ui/checkbox/Checkbox';
+import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
+import { formStyles } from '@/shared/utils/twind/styles';
+import { UserProps } from '@/shared/types/domains/user.type';
+
+export const LoginForm: FC<UserProps> = ({ credentials, onClose, onLogin }) => {
+  const { group } = useTranslation('loginForm');
+  const { login, errors, isSubmitting, handleFormSubmit } = useLoginForm({
+    credentials,
+    onSuccess: onClose,
+    onLogin,
+  });
+
+  if (!group) {
+    return null;
+  }
+
+  const isDisabled = isSubmitting;
+
+  const emailValidation = {
+    required: {
+      value: true,
+      message: group.lblEmailError,
+    },
+  };
+
+  return (
+    <form
+      onSubmit={handleFormSubmit}
+      className={formStyles.form}
+      aria-label="Login Form"
+    >
+      <div className={formStyles.formGrid}>
+        <div className={formStyles.formRow}>
+          <Label className={formStyles.label} htmlFor="txtemail">
+            {group.lblEmail}
+          </Label>
+          <Input
+            id="txtemail"
+            className="{formStyles.input}"
+            {...login('email', emailValidation)}
+            disabled={isDisabled}
+          />
+          {errors.email && (
+            <span className={formStyles.error}>{errors.email.message}</span>
+          )}
+        </div>
+      </div>
+    </form>
+  );
+};
