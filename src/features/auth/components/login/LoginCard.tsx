e637c91d77b9581cfcb6086bf7b7a7412dd17a66
Herbert Tamayo <hftamayo@gmail.com>
2025-07-22 15:50:29 -0600

    adding more imports to loginCard

--- src/features/auth/components/login/LoginCard.tsx
+++ src/features/auth/components/login/LoginCard.tsx
@@ -7,3 +7,5 @@ import {
 } from '@/shared/components/ui/card/Card';
 import { formStyles } from '@/shared/utils/twind/styles';
 import { useTranslation } from '@/shared/services/redux/hooks/useTranslation';
+import { LoginCardProps } from '@/shared/types/domains/user.type';
+import { LoginForm } from './LoginForm';
