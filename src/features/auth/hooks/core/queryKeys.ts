bf2443db55f2161a9196495cece1ef3d4257d9e1
Herbert Tamayo <hftamayo@gmail.com>
2025-07-25 14:46:43 -0600

    removing queryKeys for auth workflow

--- src/features/auth/hooks/core/queryKeys.ts
+++
@@ -1,5 +0,0 @@
-export const authKeys = {
-  login: ['auth', 'login'],
-  logout: ['auth', 'logout'],
-  register: ['auth', 'register'],
-};
--- src/features/auth/hooks/core/useAuthMutations.ts
+++ src/features/auth/hooks/core/useAuthMutations.ts
@@ -1,3 +1,3 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { authKeys } from './queryKeys';
+import { authOps } from '@/shared/services/api/apiClient';
 import { LoginProps } from '@/shared/types/domains/user.type';
