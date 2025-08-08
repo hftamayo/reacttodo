963678d921223840c5b32d0350d41996f178b986
Herbert Tamayo <hftamayo@gmail.com>
2025-08-08 15:45:13 -0600

    reanming to header container

---
+++ src/features/landing/header/HeaderContainer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HeaderLayout from './components/HeaderLayout';
+
+export const HeaderContainer: React.FC = () => {
+  return (
+    <div className="w-full h-16">
+      <HeaderLayout />
+    </div>
+  );
+};
