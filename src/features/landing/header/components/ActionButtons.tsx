799817e8bdbad97a792d14224f94509c07aab3bc
Herbert Tamayo <hftamayo@gmail.com>
2025-08-12 14:56:27 -0600

    coding and assembling ActionButtons for Landing

---
+++ src/features/landing/header/components/ActionButtons.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from '@/shared/components/ui/button/Button';
+
+export const ActionButtons: React.FC = () => {
+  return (
+    <div>
+      <Button variant="link">Login</Button>
+      <Button variant="link">Sign Up</Button>
+    </div>
+  );
+};
--- src/features/landing/header/components/HeaderLayout.tsx
+++ src/features/landing/header/components/HeaderLayout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Logo } from './Logo';
+import { ActionButtons } from './ActionButtons';
 
 export const HeaderLayout: React.FC = () => {
   return (
