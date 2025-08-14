e734fbeaffdce2c19f374446a9bb6e0a5e5cf2e9
Herbert Tamayo <hftamayo@gmail.com>
2025-08-14 09:36:00 -0600

    adding pipe separator

--- src/features/landing/header/components/ActionButtons.tsx
+++ src/features/landing/header/components/ActionButtons.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/shared/components/ui/button/Button';
 export const ActionButtons: React.FC = () => {
   return (
     <div>
-      <Button variant="link">Login</Button>
+      <Button variant="link">Login</Button>|
       <Button variant="link">Sign Up</Button>
     </div>
   );
