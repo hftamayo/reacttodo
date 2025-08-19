93a4deedaa72361c067219ae560d06643c49538d
Herbert Tamayo <hftamayo@gmail.com>
2025-08-19 11:40:30 -0600

    introducing containers for login and signup

---
+++ src/containers/Auth/LoginContainer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { LoginCard } from '@/features/auth/components/login/LoginCard';
+
+export const LoginContainer: React.FC = () => {
+  return (
+    <div>
+      <LoginCard />
+    </div>
+  );
+};
---
+++ src/containers/Auth/SignUpContainer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SignUpCard } from '@/features/auth/components/signup/SignUpCard';
+
+export const SignUpContainer: React.FC = () => {
+  return (
+    <div>
+      <SignUpCard />
+    </div>
+  );
+};
