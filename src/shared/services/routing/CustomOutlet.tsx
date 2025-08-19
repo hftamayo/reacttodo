417361a4469b09f7ed1cdacd87e7a41448577634
Herbert Tamayo <hftamayo@gmail.com>
2025-08-19 13:11:19 -0600

    using container components to route path

--- src/shared/services/routing/CustomOutlet.tsx
+++ src/shared/services/routing/CustomOutlet.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import { useRoute, Switch, Route } from 'wouter';
 import { DashBoardAnalyticsContainer } from '@/containers/DashBoard/DashBoardAnalyticsContainer';
+import { DashBoardContainer } from '@/containers/DashBoard/DashBoardContainer';
 import { TaskBoardContainer } from '@/containers/TaskBoard/TaskBoardContainer';
+import { SignUpContainer } from '@/containers/Auth/SignUpContainer';
+import { LoginContainer } from '@/containers/Auth/LoginContainer';
+import { LandingContainer } from '@/containers/Landing/LandingContainer';
 
 export const CustomOutlet: React.FC = () => {
   const [match] = useRoute('/');
 
   return (
     <Switch>
-      {match && <Route path="/" component={DashBoardAnalyticsContainer} />}
+      {/* Landing/Public Routes */}
+      <Route path="/landing" component={LandingContainer} />
+
+      {/* Authentication Routes */}
+      <Route path="/auth/login" component={LoginContainer} />
+      <Route path="/auth/signup" component={SignUpContainer} />
+
+      {/* Dashboard Routes */}
+      <Route path="/dashboard" component={DashBoardContainer} />
       <Route
-        path="/dashboard/entities/taskboard"
-        component={TaskBoardContainer}
+        path="/dashboard/analytics"
+        component={DashBoardAnalyticsContainer}
       />
-      {/* Add more routes as needed */}
+      <Route path="/dashboard/tasks" component={TaskBoardContainer} />
+
+      {/* Default Route - redirect to analytics dashboard when authenticated */}
+      {match && <Route path="/" component={DashBoardAnalyticsContainer} />}
+
+      {/* Fallback Route for unknown paths */}
+      <Route path="*" component={LandingContainer} />
     </Switch>
   );
 };
