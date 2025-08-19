1a1018e1a81d552f5545478e493390574271dc90
Herbert Tamayo <hftamayo@gmail.com>
2025-08-19 14:20:05 -0600

    activating customOutlet in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,14 @@ import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'sonner';
 import { store } from './shared/services/redux/store';
-import { LandingContainer } from './containers/Landing/LandingContainer';
-import { DashBoardContainer } from './containers/DashBoard/DashBoardContainer';
-import { AuthGuard } from './features/auth/hooks/core/AuthGuard';
 import { useAuthState } from './features/auth/hooks/core/useAuthState';
 import { GlobalModalContainer } from './shared/components/modal/GlobalModalContainer';
+import { CustomOutlet } from './shared/services/routing/CustomOutlet';
 
 const queryClient = new QueryClient();
 
 export const App: React.FC = () => {
-  const { isAuthenticated, isLoading } = useAuthState();
+  const { isLoading } = useAuthState();
 
   // Show loading state while checking authentication
   if (isLoading) {
@@ -28,13 +26,8 @@ export const App: React.FC = () => {
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <Router>
-          {isAuthenticated ? (
-            <AuthGuard>
-              <DashBoardContainer />
-            </AuthGuard>
-          ) : (
-            <LandingContainer />
-          )}
+          {/* Let CustomOutlet handle all routing logic */}
+          <CustomOutlet />
         </Router>
         <Toaster position="bottom-left" />
         <GlobalModalContainer />
