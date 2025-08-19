99e34a3662ecd90e2c3be14f78a05d2acb17416a
Herbert Tamayo <hftamayo@gmail.com>
2025-08-19 14:16:12 -0600

    slightly improve in UI in DBoardContainer

--- src/containers/DashBoard/DashBoardContainer.tsx
+++ src/containers/DashBoard/DashBoardContainer.tsx
@@ -7,8 +7,11 @@ import { MainLayout } from '../../shared/components/ui/layout/dashboard/MainLayo
  */
 export const DashBoardContainer: React.FC = () => {
   return (
-    <div>
-      <MainLayout />
-    </div>
+    <MainLayout>
+      <div className="p-6">
+        <h1 className="text-2xl font-bold text-gray-900 mb-4">Dashboard</h1>
+        <p className="text-gray-600">Welcome to your dashboard!</p>
+      </div>
+    </MainLayout>
   );
 };
