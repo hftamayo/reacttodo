da995305038fa2ffcb71b169b7b84ca9a90d4750
Herbert Tamayo <hftamayo@gmail.com>
2025-08-20 14:09:45 -0600

    temp comment password validalition

--- src/features/auth/schemas/loginSchema.ts
+++ src/features/auth/schemas/loginSchema.ts
@@ -20,11 +20,11 @@ export const loginSchema = z.object({
     .string()
     .min(1, 'Password is required')
     .min(8, 'Password must be at least 8 characters long')
-    .max(128, 'Password is too long')
-    .regex(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/,
-      'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (@$!%*?&)'
-    ),
+    .max(128, 'Password is too long'),
+  // .regex(
+  //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/,
+  //   'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (@$!%*?&)'
+  // ),
 
   rememberMe: z.boolean(),
 });
